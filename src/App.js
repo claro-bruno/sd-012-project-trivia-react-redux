12cb55feea2c4f280d20283d1b7db7733cdded0b
Patrick Dack <patrickdack89@gmail.com>
2021-08-03 17:30:02 -0300

    Requisito 1 implementado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './trivia.png';
+import Login from './component/Login';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Login />
+    );
+  }
 }
+
+export default App;
---
+++ src/component/Login.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from '../trivia.png';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+      email: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { name, email } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <div>
+            <input
+              name="name"
+              onChange={ this.handleChange }
+              value={ name }
+              type="text"
+              placeholder="Nome"
+              data-testid="input-player-name"
+            />
+            <input
+              name="email"
+              onChange={ this.handleChange }
+              value={ email }
+              type="email"
+              placeholder="Email"
+              data-testid="input-gravatar-email"
+            />
+          </div>
+          <button type="button" data-testid="btn-play" disabled={ name.length === 0 || email.length === 0 }>Jogar</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Login;
