42e84121b0769de680a107e8d17b045eae56afa7
MGSiqueira <51840153+MGSiqueira@users.noreply.github.com>
2021-08-03 20:44:28 -0300

    Requisito 4 (#120)

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import md5 from 'crypto-js/md5';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.hash = this.hash.bind(this);
+  }
+
+  hash() {
+    const { email } = this.props;
+    const hash = md5(email).toString();
+    return hash;
+  }
+
+  render() {
+    const { name } = this.props;
+    return (
+      <div>
+        <img
+          src={ `https://www.gravatar.com/avatar/${this.hash()}` }
+          alt="Player"
+          data-testid="header-profile-picture"
+        />
+        <div data-testid="header-player-name">{ name }</div>
+        <div data-testid="header-score">0</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.email,
+  name: state.name,
+});
+
+export default connect(mapStateToProps, null)(Header);
+
+Header.propTypes = {
+  email: PropTypes.string,
+  name: PropTypes.string,
+}.isRequired;
