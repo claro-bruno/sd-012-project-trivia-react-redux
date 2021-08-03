1a3f4058fb96c26cc242b781a568992600d6807e
Eduardo Linhares <40336411+DuaarDor@users.noreply.github.com>
2021-08-03 18:03:43 -0300

    Passa provide para index.js
    * Update README.md
    
    * Passa provide para index.js
    
    Co-authored-by: Henrique Jensen <henriquejensen@hotmail.com>

---
+++ src/Settings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <header className="main-header">
+        <h1 className="logo">BRAINTEST</h1>
+        <h1 data-testid="settings-title">Configurações</h1>
+      </header>
+    );
+  }
+}
+
+export default Settings;
