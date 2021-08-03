3accc2fce02cdb736b0d83a6492df2ace15c8d2d
Caio Morato <morato_dev@protonmail.com>
2021-08-03 20:12:51 -0300

    ajustes do lint do requisito 4, pronto pra merge

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
+import Login from './pages/Login';
 import './App.css';
 
-export default function App() {
-  return (
-    <div className="App" />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    );
+  }
 }
+
+export default App;
