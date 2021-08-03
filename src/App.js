4b7646b6034c12d5ab980d150db4f90c8344e5a2
RenanMiyashita <renanmiyashita@gmail.com>
2021-08-03 19:06:55 -0300

    grupo-35-requisito-1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import logo from './trivia.png';
-import HomePage from './pages/HomePage';
+import Login from './pages/Login';
 import './App.css';
 
 export default function App() {
   return (
-    <div>
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-        </header>
-      </div>
+    // <div>
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={ logo } className="App-logo" alt="logo" />
+    //     </header>
+    //   </div>
+    <>
       <Switch>
-        <Route exact path="/" component={ HomePage } />
+        <Route exact path="/" component={ Login } />
       </Switch>
-    </div>
+      <div />
+    </>
   );
 }
