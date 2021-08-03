b5988a923eac7fc42ca9e05b4a71c7c7e6eb5c30
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-08-03 18:06:45 -0300

    corrige nomes de variaveis

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,30 @@ import logo from '../trivia.png';
 import Input from '../components/Input';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  buttonDisable() {
+    const { email, name } = this.state;
+    if (email.length > 0 && name.length > 0) return false;
+    return true;
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,14 +39,23 @@ class Login extends Component {
               name="name"
               type="text"
               testid="input-player-name"
+              onChange={ this.handleChange }
+
             />
             <Input
-              id="password"
-              name="password"
-              type="password"
+              id="email"
+              name="email"
+              type="email"
               testid="input-gravatar-email"
+              onChange={ this.handleChange }
             />
-            <button type="button" data-testid="btn-play">Jogar</button>
+            <button
+              type="button"
+              data-testid="btn-play"
+              disabled={ this.buttonDisable() }
+            >
+              Jogar
+            </button>
           </p>
         </header>
       </div>
