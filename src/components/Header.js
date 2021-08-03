f51bdce5f00bd79932d6aadd28d7a0624e8204e7
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-08-03 17:10:38 -0300

    cria cria componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
-  render(){
+  render() {
+    const { profilePicture, name, score } = this.props;
     return (
       <header>
-        <img src={}
+        <img
+          data-testid="header-profile-picture"
+          src={ profilePicture }
+          alt="foto-perfil"
+        />
+        <span data-testid="header-player-name">{ name }</span>
+        <span data-testid="header-score">{ score }</span>
       </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  profilePicture: state.user.profilePicture,
+  name: state.user.name,
+  score: state.trivia.score,
+});
+
+Header.propTypes = {
+  profilePicture: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
