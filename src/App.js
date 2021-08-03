c3fecd82fde8357415bce93b0d0f4d528380d848
Luiz Henrique <luishenriqueee2010@hotmail.com>
2021-08-03 16:55:36 -0300

    Estrutura App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import logo from './trivia.png';
-import './App.css';
+import { Component, React } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Feedback from './pages/Feedback';
+import Login from './pages/Login';
+import Ranking from './pages/Ranking';
+import Settings from './pages/Settings';
+import Trivia from './pages/Trivia';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/settings" component={ Settings } />
+          <Route exact path="/trivia" component={ Trivia } />
+          <Route exact path="/feedback" component={ Feedback } />
+          <Route exact path="/ranking" component={ Ranking } />
+        </Switch>
+      </div>
+    );
+  }
 }
+
+export default App;
