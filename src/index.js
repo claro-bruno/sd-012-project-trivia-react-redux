a3d47d275f057a49de99d916a9dfe5511149a734
Eduardo Linhares <40336411+DuaarDor@users.noreply.github.com>
2021-08-03 18:15:17 -0300

    Revert 56 main group 8 provider eduardo linhares (#62)
    * Revert "Passa provide para index.js"
    
    This reverts commit 1a3f4058fb96c26cc242b781a568992600d6807e.
    
    * Adiciona provider
    
    * Agora vai

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './redux/store';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+  <React.StrictMode>
+    <Provider store={ store }>
+      <App />
+    </ Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PW
 serviceWorker.unregister();
