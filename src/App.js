7fe97c59875b0d26f6e36f4925a9fbdb2def54e4
Pedro Resck <pedroresck1@gmail.com>
2021-08-03 19:07:42 -0300

    Requisito 3 - criar um botao na pagina inicial que leve para a tela de configuracoes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Config from './pages/Config';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/config" component={ Config } />
       </Switch>
     </div>
   );
