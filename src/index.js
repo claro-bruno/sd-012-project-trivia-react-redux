4afad1880954fdc8461474f109811e305d360152
RyanLaiber <ryanlaiber@gmail.com>
2021-08-03 18:30:55 -0300

    implementa pagina de Login e Routers no App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowseRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
@@ -8,11 +8,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowseRouter>
+  <BrowserRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowseRouter>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 serviceWorker.unregister();
---
+++ src/pages/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from '../trivia.png';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+      email: '',
+    }
+  }
+
+  handleChange({ target }) {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
+    });
+  }
+
+  render() {
+    const { name, email } = this.state;
+    return (
+      <main>
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+        </header>
+        <form>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            id="name"
+            data-testid="input-player-name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+          <label htmlFor="email">E-mail:</label>
+          <input
+            type="email"
+            id="email"
+            data-testid="input-gravatar-email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+          <button type="button" data-testid="btn-play">Jogar</button>
+        </form>
+      </main>
+    )
+  }
+}
+
+export default Login;
