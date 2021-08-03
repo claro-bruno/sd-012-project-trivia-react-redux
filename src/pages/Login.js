62110c0a8defec8d10cf9cd7f62b28ecd3fbafc5
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-08-03 17:31:50 -0300

    lucas-fernandes-requisito01

---
+++ src/pages/Login.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      disableBtn: true,
+      nameInput: '',
+    };
+    this.validateEmail = this.validateEmail.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validateEmail(emailValue) {
+    const reg = /^[a-z0-9_.-]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/;
+    this.setState({
+      disableBtn: (!reg.test(emailValue)),
+    });
+  }
+
+  handleChange(target) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const minLengthName = 3;
+    const { disableBtn, nameInput } = this.state;
+    return (
+      <section>
+        <label htmlFor="name-input">
+          Nome
+          <input
+            id="name-input"
+            name="nameInput"
+            data-testid="input-player-name"
+            onChange={ ({ target }) => this.handleChange(target) }
+          />
+        </label>
+        <label htmlFor="email-input">
+          <input
+            id="email-input"
+            data-testid="input-gravatar-email"
+            onChange={ ({ target }) => this.validateEmail(target.value) }
+          />
+        </label>
+        <Link to="/jogo">
+          <button
+            type="button"
+            data-testid="btn-play"
+            variant="contained"
+            color="primary"
+            disabled={ disableBtn || nameInput.length < minLengthName }
+          >
+            Jogar
+          </button>
+        </Link>
+      </section>
+    );
+  }
+}
+
+export default Login;
