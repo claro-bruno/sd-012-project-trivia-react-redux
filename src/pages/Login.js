efdebf1984fdbccf25064ccf2c22424961715282
Pedro <peg.gep27@gmail.com>
2021-08-03 17:58:20 -0300

    juncao dos logins

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,18 +5,73 @@ import { Link } from 'react-router-dom';
 import { fetchToken } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.btnCondition = this.btnCondition.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  btnCondition() {
+    const { name, email } = this.state;
+    return !(name !== '' && email !== '');
+  }
+
   render() {
     const { getToken } = this.props;
     return (
-      <div>
-        <Link
-          onClick={ () => {
-            getToken();
-          } }
-          to="/trivia"
-        >
-          Jogar
-        </Link>
+
+      <div className="inputs">
+        <form className="forms">
+          <label htmlFor="name-input" className="label-input">
+            Nome:
+            <input
+              data-testid="input-player-name"
+              id="name-input"
+              name="name"
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="email-input" className="label-input">
+            E-mail:
+            <input
+              data-testid="input-gravatar-email"
+              id="email-input"
+              type="email"
+              name="email"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <div className="btn">
+            <button
+              className="btn2"
+              data-testid="btn-play"
+              type="button"
+              disabled={ this.btnCondition() }
+            >
+              <Link
+                onClick={ () => {
+                  getToken();
+                } }
+                to="/trivia"
+              >
+                Jogar
+              </Link>
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
