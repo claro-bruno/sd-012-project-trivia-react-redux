3ee4cb311540482a97f7a44efba363350d42c957
Pedro <peg.gep27@gmail.com>
2021-08-03 18:31:14 -0300

    Finaliza requisito 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,10 @@
-import { GET_TOKEN, GET_TOKEN_SUCCESS, GET_QUESTIONS,
-  GET_QUESTIONS_SUCCESS } from './actionsType';
-
-export const getToken = () => ({ type: GET_TOKEN });
-
-export function getTokenSuccess(token) {
-  return { type: GET_TOKEN_SUCCESS, payload: token };
-}
+import { GET_QUESTIONS, GET_QUESTIONS_SUCCESS } from './actionsType';
 
 export const getQuestions = () => ({ type: GET_QUESTIONS });
 
 export function getQuestionsSuccess(questions) {
   return { type: GET_QUESTIONS_SUCCESS, payload: questions };
 }
-
-export const fetchToken = () => async (dispatch) => {
-  dispatch(getToken());
-  const response = await fetch('https://opentdb.com/api_token.php?command=request');
-  const { token } = await response.json();
-  dispatch(getTokenSuccess(token));
-};
 
 export const fetchQuestions = (token) => async (dispatch) => {
   dispatch(getQuestions());
