5ca68c78805c9162c4187b0e708ce0a95a1d1355
Adriana Biberg <adribiberg@hotmail.com>
2021-08-03 18:01:27 -0300

    Cumpre requisito 1 - pagina de login com form de login

--- src/redux/reducers/reducer.jsx
+++ src/redux/reducers/reducer.jsx
@@ -1,16 +1,16 @@
 // reducers/reducer.jsx
 
-import { ACTION_TEST } from "../actions";
+// import { ACTION_TEST } from "../actions";
 
 const INITIAL_STATE = {};
 
 const reducer = (state = INITIAL_STATE, action) => {
-	switch (action.type) {
-		case ACTION_TEST:
-			return { ...state, action.data }
-		default:
-			return state
-	}
-}
+  switch (action.type) {
+  // case ACTION_TEST:
+  // return { ...state, action.data }
+  default:
+    return state;
+  }
+};
 
 export default reducer;
