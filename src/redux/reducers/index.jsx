7e94cddb23418b14a419e785aa7ba6cc728883cd
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-08-03 17:06:19 -0300

    Configuração do Redux

---
+++ src/redux/reducers/index.jsx
@@ -0,0 +1,7 @@
+// reducers/index.jsx
+
+import { combineReducers } from 'redux';
+import reducer from './reducer';
+
+const rootReducers = combineReducers({ reducer });
+export default rootReducers;
