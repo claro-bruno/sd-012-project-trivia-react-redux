a1869fa4ec1a381c224ba2443ac1d386537a0bc4
Roberval Filho <rslfilho@gmail.com>
2021-08-03 20:52:06 -0300

    Pegando acertos do state e fazendo ternário da msg

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { assertions } = this.props;
+    const NUMBER_THREE = 3;
+
     return (
       <>
         <Header />
-        Feedback
+        <p data-testid="feedback-text">
+          {
+            assertions < NUMBER_THREE ? 'Podia ser melhor...' : 'Mandou bem!'
+          }
+        </p>
       </>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  assertions: state.gameReducer.assertions, // nome provisório aguardando a criação do reducer do jogo
+});
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+};
