9c09171db4efe7252748163be17ad747deea9ba4
Caio Morato <morato_dev@protonmail.com>
2021-08-03 18:39:45 -0300

    estilizaçao base header feito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import md5 from 'crypto-js/md5';
 import '../styles/Header.css';
 
 class Header extends React.Component {
   render() {
+    const userMailHashCode = md5('email-que-virá-do-state@trybe.com').toString();
     return (
       <header className="standard-header">
         <div className="player-info">
           <img
             data-testid="header-profile-picture"
-            src="info/vinda/do/store"
+            src={ `https://www.gravatar.com/avatar/${userMailHashCode}` }
             alt="avatar do jogador"
+            className="player-image"
           />
-          <span>
+          <span className="jogador-span">
             Jogador:
-            <span data-testid="header-player-name" />
+            <span className="player-name" data-testid="header-player-name" />
             Nuwanda
           </span>
         </div>
         <div className="player-score">
           <span>
             Pontuação:
-            <span data-testid="header-score">
+            <span className="player-score-number" data-testid="header-score">
               20
             </span>
           </span>
