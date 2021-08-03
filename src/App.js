756bfa84994f5ec6baf723797b705bcfd9cf8bfb
ArturAlvaro <artur.alvaro73@gmail.com>
2021-08-03 18:21:24 -0300

    Prepara ambiente

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import logo from './trivia.png';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+        </header>
+      </div>
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+      </Switch>
     </div>
   );
 }
