9ee8a93610634e74e0e556f6991f77aff545577b
caio-cesar-lopes <82236141+caio-cesar-lopes@users.noreply.github.com>
2021-08-04 20:50:16 -0300

    [Group 23] [Caio Lopes] Requisito 5. (#242)
    * req.5
    
    * ajustes solicitados no code review

---
+++ src/components/Questions.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+class Questions extends Component {
+  constructor() {
+    super();
+    this.state = {
+      questions: [],
+    };
+    this.getUnities = this.getUnities.bind(this);
+  }
+
+  componentDidMount() {
+    this.getUnities();
+  }
+
+  async getUnities() {
+    const token = localStorage.getItem('token');
+    const API_URL = await fetch(`https://opentdb.com/api.php?amount=5&token${token}`);
+    const questions = await API_URL.json();
+
+    this.setState(() => ({
+      questions: questions.results,
+    }));
+  }
+
+  incorrectAnswers() {
+    const { questions } = this.state;
+    const getQuestion = questions[0];
+    const selectedQuestion = getQuestion && getQuestion.incorrect_answers;
+    return selectedQuestion && selectedQuestion.map((question, index) => (
+      <button
+        type="button"
+        key={ question }
+        data-testid={ `wrong-answer-${index}` }
+      >
+        {question}
+      </button>));
+  }
+
+  render() {
+    const { questions } = this.state;
+    const selectedQuestion = questions[0];
+    return (
+      <div>
+        <h1 data-testid="question-category">
+          {selectedQuestion && selectedQuestion.category}
+        </h1>
+        <p data-testid="question-text">
+          {selectedQuestion && selectedQuestion.question}
+        </p>
+        <button data-testid="correct-answer" type="button">
+          {selectedQuestion && selectedQuestion.correct_answer}
+        </button>
+        {this.incorrectAnswers()}
+      </div>
+    );
+  }
+}
+
+export default Questions;
