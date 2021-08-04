ee89ba02baf143287faefd96ffc96f19fcb31670
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-08-04 15:31:15 -0300

    Cria funcao fetchApi e page game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
+import Game from './Pages/Game';
 import Login from './Pages/Login';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route path="/game" component={ Game } />
         <Route exact path="/" component={ Login } />
       </Switch>
     );
--- src/redux/actions/index.jsx
+++ src/redux/actions/index.jsx
@@ -1,5 +1,33 @@
-// actions/index.jsx
+import fetchApiGame from '../../services/fetchApiGame';
 
-export const ACTION_TEST = 'ACTION_TEST';
+export const REQUEST_API_GAME_LOADING = 'REQUEST_API_GAME';
+export const REQUEST_API_GAME_SUCCESS = 'REQUEST_API_GAME_SUCCESS';
+export const REQUEST_API_GAME_ERROR = 'REQUEST_API_GAME_ERROR';
 
-export const action = (data) => ({ type: ACTION_TEST, data });
+export const actionRequestApiGame = () => ({
+  type: REQUEST_API_GAME_LOADING,
+  isFetching: true,
+});
+
+export const actionRequestApiGameSuccess = (data) => ({
+  type: REQUEST_API_GAME_SUCCESS,
+  isFetching: false,
+  payload: data,
+});
+
+export const actionRequestApiGameError = (error) => ({
+  type: REQUEST_API_GAME_ERROR,
+  isFetching: false,
+  error,
+});
+
+export const actionFetchApiGame = () => async (dispatch) => {
+  dispatch(actionRequestApiGame());
+  try {
+    const fetchApi = await fetchApiGame();
+    dispatch(actionRequestApiGameSuccess(fetchApi));
+  } catch (error) {
+    console.log(error);
+    dispatch(actionRequestApiGameError(error));
+  }
+};
