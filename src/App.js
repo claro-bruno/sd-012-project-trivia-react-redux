9f7575200a381a0f38e7973f4d64f81cadc815dc
Roberval Filho <78314158+rslfilho@users.noreply.github.com>
2021-08-04 06:37:05 -0300

    [Group 23] [Roberval] - Requisito 13 (#125)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
 import './App.css';
+import Feedback from './pages/Feedback';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route path="/feedback" component={ Feedback } />
         <Route exact path="/" component={ Login } />
       </Switch>
     );
