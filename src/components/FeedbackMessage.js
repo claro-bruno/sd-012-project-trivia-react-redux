6893f09780a74214567406b35dfa492026babb4c
Euller Braz <euller.braz99@gmail.com>
2021-08-04 18:22:39 -0300

    Implementado mensagem de feedback

---
+++ src/components/FeedbackMessage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class FeedbackMessage extends React.Component {
+  render() {
+    const { playerScore } = this.props;
+    const goodPerfomanceNumber = 3;
+    return (
+      <h1
+        data-testid="feedback-text"
+      >
+        {
+          playerScore < goodPerfomanceNumber
+            ? 'Podia ser melhor...'
+            : 'Mandou bem!'
+        }
+      </h1>
+    );
+  }
+}
+
+FeedbackMessage.propTypes = {
+  playerScore: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  playerScore: state.game.score,
+});
+
+export default connect(mapStateToProps)(FeedbackMessage);
