97ef4b02985438ee63bc8705b84ed0a01bdbc373
igor <igorsife80@gmail.com>
2021-08-04 18:15:16 -0300

    requisito-4-completo

--- src/components/HeaderGame.jsx
+++ src/components/HeaderGame.jsx
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 
 class HeaderGame extends Component {
+  constructor(props) {
+    super(props);
+    this.handleData = this.handleData.bind(this);
+    this.state = {
+      email: '',
+      name: '',
+      score: 0,
+    };
+  }
+
+  componentDidMount() {
+    // const { getEndPoint } = this.props;
+    this.handleData();
+    // const endPonit = `https://www.gravatar.com/avatar/${hash}`;
+    // getEndPoint(endPonit);
+  }
+
+  handleData() {
+    const { user: { email, name } } = this.props;
+    this.setState({
+      email,
+      name,
+    });
+  }
+
   render() {
+    const { email } = this.state;
+    const hash = md5(email).toString().toLowerCase();
+    const { name, score } = this.state;
     return (
       <>
-        i ai vei blz ?
+        <img
+          data-testid="header-profile-picture"
+          src={ `https://www.gravatar.com/avatar/${hash}` }
+          alt="imgem-do-avatar"
+        />
+        <p data-testid="header-player-name">{name}</p>
+        <p data-testid="header-score">{score}</p>
       </>
     );
   }
 }
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
 
-export default HeaderGame;
+export default connect(mapStateToProps)(HeaderGame);
