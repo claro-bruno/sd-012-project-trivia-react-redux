5fa0bba0051f57775356aeba8020dec036b9657d
Marcela Silva <marcela.ops@hotmail.br>
2021-08-03 21:14:09 -0300

    [3] fix: pequena mudança

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -4,7 +4,9 @@ class Settings extends React.Component {
   render() {
     return (
       <div>
-        <h1 data-testid="settings-title"> Configurações </h1>
+        <h1 data-testid="settings-title">
+          Configurações
+        </h1>
       </div>
     );
   }
