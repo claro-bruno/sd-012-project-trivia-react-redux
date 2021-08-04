f3fb24ad0060f860b5c15a8406b6755d6947f46e
Stefano <stefanooliveira1@gmail.com>
2021-08-04 17:22:59 -0300

    vamo linter

--- src/Components/ConfigButton.js
+++ src/Components/ConfigButton.js
@@ -6,14 +6,14 @@ class ConfigButton extends Component {
     const { push } = this.props;
     return (
       <button
-      type="button"
-      data-testid="btn-settings"
-      onClick={ () => {
-        push('/configs');
-      } }
-    >
-      Configurações
-    </button>
+        type="button"
+        data-testid="btn-settings"
+        onClick={ () => {
+          push('/configs');
+        } }
+      >
+        Configurações
+      </button>
     );
   }
 }
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ConfigButton from '../Components/ConfigButton'
+import ConfigButton from '../Components/ConfigButton';
 // import logo from '../trivia.png';
 
 class Login extends React.Component {
