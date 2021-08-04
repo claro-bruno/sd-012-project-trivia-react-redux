ca9c9f254e0958c4d7c8c117f6ab75965e828973
Martin <escorpmartin97@gmail.com>
2021-08-04 16:48:37 -0300

    requisito 04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import logo from './trivia.png';
 import './App.css';
-import Routes from './Routes';
 
 export default function App() {
   return (
-    <div>
-
-      <Routes />
+    <div className="App">
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>
+          SUA VEZ
+        </p>
+      </header>
     </div>
   );
 }
