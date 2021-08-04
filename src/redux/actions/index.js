52f2d6760ed92c9a02c2b937bff408e35c4f0122
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-04 11:48:49 -0300

    cria action tokenTriviaAction

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,8 @@
 export { ADD_LOGIN, addLoginAction } from './addLoginAction';
+
+export {
+  GET_API_TRIVIA_TOKEN,
+  GET_API_TRIVIA_TOKEN_SUCESS,
+  GET_API_TRIVIA_TOKEN_ERROR,
+  tokenFetchAPI,
+} from './tokenTriviaAction';
