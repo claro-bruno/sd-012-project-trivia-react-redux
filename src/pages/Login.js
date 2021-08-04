9025295b308efe93a094c53a1f9d7856240b9442
Marcela Silva <marcela.ops@hotmail.br>
2021-08-04 17:08:08 -0300

    fix: [3] butn Configurações em `Login.js`

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -61,6 +61,11 @@ class Login extends React.Component {
             Jogar
           </button>
         </Link>
+        <Link to="/settings">
+          <button type="button" data-testid="btn-settings">
+            Configurações
+          </button>
+        </Link>
       </form>
     );
   }
