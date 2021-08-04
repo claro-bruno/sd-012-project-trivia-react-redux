745078dfd01215485b85e6e2d4789d64c356570f
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-04 16:54:45 -0300

    cria reducer gravatarReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import addLoginReducer from './addLoginReducer';
 import tokenTriviaReducer from './tokenTriviaReducer';
 import questionsTriviaReducer from './questionsTriviaReducer';
+import gravatarReducer from './gravatarReducer';
 
 const rootReducer = combineReducers({
   addLoginReducer,
   tokenTriviaReducer,
   questionsTriviaReducer,
+  gravatarReducer,
 });
 
 export default rootReducer;
