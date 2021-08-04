a3b25e8a79789d7233566a8bd7bd9d11c1d8670e
CaioMorato <68545727+CaioMorato@users.noreply.github.com>
2021-08-04 20:37:09 -0300

    [Grupo 23] [Nuwanda] - Requisito 17 e 18 (#209)
    * montagem do Ranking, pegando dados de local storage e tratamento caso seja null + estilizaçao base
    
    * ordenar por pontuaçao feito
    
    * fazendo e estilizando o botao pra ir pra home
    
    * ajustes no css pra nao dar pau no lint:styles
    
    * Update Ranking.js
    
    * ajuste lint
    
    * colocando componentes jsx em funçoes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
-import Quiz from './pages/Quiz';
-import './App.css';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
+import Quiz from './pages/Quiz';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/ranking" component={ Ranking } />
-        <Route path="/feedback" component={ Feedback } />
         <Route exact path="/" component={ Login } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route exact path="/ranking" component={ Ranking } />
         <Route path="/quiz" component={ Quiz } />
       </Switch>
     );
