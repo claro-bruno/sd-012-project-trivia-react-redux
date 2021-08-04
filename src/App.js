5380b1596eda9e4909e10729e569ba3e30894d71
Roberval Filho <rslfilho@gmail.com>
2021-08-04 14:53:56 -0300

    16 - Criando rota para ranking

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
 import './App.css';
 import Feedback from './pages/Feedback';
+// import Ranking from './pages/Ranking';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
+        <Route path="/ranking" component={ Ranking } />
         <Route path="/feedback" component={ Feedback } />
         <Route exact path="/" component={ Login } />
       </Switch>
