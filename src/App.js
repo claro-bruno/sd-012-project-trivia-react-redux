281492767175eb5d2a21ab58238f1d13981648d5
Euller Braz <euller.braz99@gmail.com>
2021-08-04 14:38:09 -0300

    Renderizado Header no page Game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
+import Feedback from './pages/Feedback';
 
 export default class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route path="/" component={ Login } />
+        <Route exact path="/" component={ Login } />
+        <Route path="/game" component={ Game } />
+        <Route path="/feedback" component={ Feedback } />
       </Switch>
     );
   }
