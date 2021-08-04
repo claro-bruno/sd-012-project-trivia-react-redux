8ead1fee17ed5fdd60612d2baae5d3f915ecefd4
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-04 15:40:09 -0300

    implementa questionsTriviaReducer no rootReducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import addLoginReducer from './addLoginReducer';
 import tokenTriviaReducer from './tokenTriviaReducer';
+import questionsTriviaReducer from './questionsTriviareducer';
 
 const rootReducer = combineReducers({
   addLoginReducer,
   tokenTriviaReducer,
+  questionsTriviaReducer,
 });
 
 export default rootReducer;
