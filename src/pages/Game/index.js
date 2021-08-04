ea3b5fc02c8a768c9b92afa234c6f1c0181f6ffe
Martin <escorpmartin97@gmail.com>
2021-08-04 19:58:48 -0300

    [Requisito 5 e 6]

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -3,24 +3,59 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5'; // Utilizando biblioteca CryptoJS conforme README.md;
 
+import ActualQuestion from './ActualQuestion';
+
 class Game extends Component {
+  constructor() {
+    super();
+    this.getQuestions = this.getQuestions.bind(this);
+    this.state = {
+      questions: [],
+      questionIndex: 0,
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getQuestions();
+  }
+
+  getQuestions() {
+    const token = localStorage.getItem('token');
+    const endpoind = `https://opentdb.com/api.php?amount=5&token=${token}`;
+    fetch(endpoind)
+      .then((response) => response.json())
+      .then(({ results }) => this.setState({ questions: results, loaded: true }));
+  }
+
   render() {
+    const { questions, questionIndex, loaded } = this.state;
     const { gravatarEmail, name } = this.props;
 
     // Passando email para formato md5 de criptografia;
     const encodeEmail = md5(gravatarEmail).toString();
 
     return (
-      <header>
-        <img
-          // url da foto do usuário, com o formato que está no README.md;
-          src={ `https://www.gravatar.com/avatar/${encodeEmail}` }
-          alt="profile"
-          data-testid="header-profile-picture"
-        />
-        <span data-testid="header-player-name">{ name }</span>
-        <span data-testid="header-score">0</span>
-      </header>
+      <div>
+        <header>
+          <img
+            // url da foto do usuário, com o formato que está no README.md;
+            src={ `https://www.gravatar.com/avatar/${encodeEmail}` }
+            alt="profile"
+            data-testid="header-profile-picture"
+          />
+          <span data-testid="header-player-name">{ name }</span>
+          <span data-testid="header-score">0</span>
+        </header>
+        <main>
+          { loaded
+          && (
+            <ActualQuestion
+              question={ questions[questionIndex] }
+            />
+          ) }
+        </main>
+      </div>
     );
   }
 }
