1e4d6de0ba8f07f8d9b3b7483034855917d33b66
Raphael <66336767+Raph2ll@users.noreply.github.com>
2021-08-04 15:11:47 -0300

    Revert "Criando page (#170)"
    This reverts commit 843928fb3eb3f75748c7dff7e4cf6b59ae0ef8a7.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import { Login, Game, Settings } from './pages/index';
+import Login from './pages/Login';
+import Header from './components/Header';
+import Question from './components/Question';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/game" component={ Game } />
+        <Route path="/quiz" component={ Question } />
         <Route exact path="/settings" component={ Settings } />
       </Switch>
     </div>
--- src/components/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as Header } from './Header';
-export { default as InputCard } from './InputCard';
-export { default as Question } from './Question';
