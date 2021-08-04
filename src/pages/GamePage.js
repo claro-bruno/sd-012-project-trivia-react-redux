da51edd907a76cbcda2c01bef449353e2538fd6b
Adryan Rosa <adryanrosa374@gmail.com>
2021-08-04 00:13:03 -0300

    Importa PropTypes faltando

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import GameQuestions from '../components/GameQuestions';
 import HeaderPlayer from '../components/HeaderPlayer';
 
