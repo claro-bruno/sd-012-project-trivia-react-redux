70d9fab67a253713e2be2ab6108b88f58dce3de5
camila-mp <ca.malvp@gmail.com>
2021-08-04 09:32:53 -0300

    edita actions, reducers e componente Login para permitir requisiçao a API e armazenagem do token

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,23 @@
 export const LOGIN_ACTION = 'LOGIN_ACTION';
+export const REQUEST_TOKEN = 'REQUEST_TOKEN';
+export const RECEIVE_TOKEN = 'RECEIVE_TOKEN';
+export const ERROR_TOKEN = 'ERROR_TOKEN';
 
 export const loginAction = (name, email) => ({
   type: LOGIN_ACTION,
   name,
   email,
+});
+
+export const requestToken = () => ({
+  type: REQUEST_TOKEN,
+});
+
+export const receiveToken = (data) => ({
+  type: RECEIVE_TOKEN,
+  data,
+});
+
+export const errorToken = () => ({
+  type: ERROR_TOKEN,
 });
--- src/reducers/Player.js
+++ src/reducers/Player.js
@@ -1,9 +1,12 @@
-import { LOGIN_ACTION } from '../actions';
+import { LOGIN_ACTION, REQUEST_TOKEN, RECEIVE_TOKEN, ERROR_TOKEN } from '../actions';
 
 const INITIAL_STATE = {
   player: {
     playerName: '',
     playerEmail: '',
+    token: '',
+    isLoading: false,
+    error: false,
   },
 };
 
@@ -16,6 +19,21 @@ const PlayerReducer = (state = INITIAL_STATE, action) => {
         playerName: '',
         playerEmail: '',
       },
+    };
+  case REQUEST_TOKEN:
+    return {
+      ...state,
+      isLoading: true,
+    };
+  case RECEIVE_TOKEN:
+    return {
+      ...state,
+      data: action.data,
+    };
+  case ERROR_TOKEN:
+    return {
+      ...state,
+      error: true,
     };
   default:
     return state;
