d37f7c72293c574d83782527a43eb123e10744d8
Caio Morato <morato_dev@protonmail.com>
2021-08-04 20:24:31 -0300

    colocando componentes jsx em funçoes

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import '../styles/Ranking.css';
 
 class Ranking extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.scoreBoard = this.scoreBoard.bind(this);
+    this.noStorageData = this.noStorageData.bind(this);
+  }
+
+  scoreBoard() {
     const localStorageData = JSON.parse(localStorage.getItem('ranking'));
-
-    // Colocando o elemento do ranking em uma variável pra usar renderização condicional.
-    // Caso o localStorageData não tenha nenhum dado, ele recebe null então retorna um erro ao fazer o map
-
-    const scoreBoard = (
+    return (
       <div className="score-board">
         {localStorageData && localStorageData
           .sort((a, b) => b.score - a.score)
@@ -24,12 +26,22 @@ class Ranking extends React.Component {
             </ul>
           ))}
       </div>);
+  }
 
-    const noStorageData = <span>Sem pontuações no momento.</span>;
+  noStorageData() {
+    return <span>Sem pontuações no momento.</span>;
+  }
+
+  render() {
+    const localStorageData = JSON.parse(localStorage.getItem('ranking'));
+
+    // Colocando o elemento do ranking em uma variável pra usar renderização condicional.
+    // Caso o localStorageData não tenha nenhum dado, ele recebe null então retorna um erro ao fazer o map
+
     return (
       <div className="ranking-page">
         <h1 data-testid="ranking-title">Ranking</h1>
-        {localStorageData ? scoreBoard : noStorageData}
+        {localStorageData ? this.scoreBoard() : this.noStorageData()}
         <Link className="ranking-button-container" to="/">
           <button data-testid="btn-go-home" className="ranking-button" type="button">
             Página de Login
