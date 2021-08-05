fe7df06d5b19be3bb3d6f5127f8c7b4d0edab63e
zLibrian <kevinwillian084@gmail.com>
2021-08-05 00:22:33 -0300

    Some changes on reducers to make the login one works

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import player from './player';
+import login from './login';
 
-const rootReducer = combineReducers({ player });
+const rootReducer = combineReducers({ player, login });
 
 export default rootReducer;
