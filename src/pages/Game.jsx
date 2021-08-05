25c2e9471f0a5554494355a1ecee52ccda5baf11
Teika <shimworks.tk@gmail.com>
2021-08-05 16:44:38 -0300

    testa alteraçoes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getTriviaAPI } from '../redux/action/index';
 
 class Game extends Component {
   constructor(props) {
@@ -6,11 +9,41 @@ class Game extends Component {
     this.state = { };
   }
 
+  componentDidMount() {
+    const { getAPI } = this.props;
+    const token = JSON.parse(localStorage.getItem('token'));
+    getAPI(token);
+  }
+
   render() {
+    const { questions } = this.props;
+    console.log(questions);
+    // const token = JSON.parse(localStorage.getItem('token'));
+    // getAPI(token);
     return (
-      <div>renderiza isso por enquanto</div>
+      <button type="button">um botao</button>
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+  questions: state.questions.results,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  getAPI: (token) => dispatch(getTriviaAPI(token)),
+});
+
+Game.propTypes = {
+  getAPI: PropTypes.func.isRequired,
+  questions: PropTypes.shape({
+    results: PropTypes.shape({
+      category: PropTypes.string,
+      question: PropTypes.string,
+      correctAnswer: PropTypes.string,
+      incorrectAnswers: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Game);
---
+++ src/redux/reducer/questions.js
@@ -0,0 +1,27 @@
+const STATE_INITIAL = ({
+  response_code: 0,
+  results: [
+    {
+      category: '',
+      type: '',
+      difficulty: '',
+      question: '',
+      correctAnswer: '',
+      incorrectAnswers: [],
+    },
+  ],
+});
+
+const questions = (state = STATE_INITIAL, action) => {
+  switch (action.type) {
+  case 'QUESTION':
+    return {
+      ...state,
+      results: [action.value],
+    };
+  default:
+    return state;
+  }
+};
+
+export default questions;
