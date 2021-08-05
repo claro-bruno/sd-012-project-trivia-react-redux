72e9097f58450cba716bb0328787c5770b5f2f37
Eric Kreis <erickreis793@gmail.com>
2021-08-04 23:55:35 -0300

    [Requisito 08 - Cria timer de 30 segundos para responder]

--- src/pages/Game/ActualQuestion/index.js
+++ src/pages/Game/ActualQuestion/index.js
@@ -6,10 +6,37 @@ class ActualQuestion extends Component {
   constructor() {
     super();
     this.handleChangeStyle = this.handleChangeStyle.bind(this);
+    this.counter = this.counter.bind(this);
 
     this.state = {
       answered: false,
+      timer: 30,
     };
+  }
+
+  // No momento que o componente é montado inicia o timer;
+  componentDidMount() {
+    this.counter();
+  }
+
+  // Conforme a atualização do componente, caso o timer chegue a zero é removido;
+  componentDidUpdate() {
+    const { timer } = this.state;
+    if (timer === 0) {
+      clearInterval(this.interval);
+    }
+  }
+
+  counter() {
+    const oneSecond = 1000; // Tempo de um segundo em milisegundos;
+    const interval = 30000; // 30 segundos em milisegundos;
+
+    // Após 30 segundos mudará o estado representando que foi respondido;
+    setTimeout(() => this.setState({ answered: true }), interval);
+    // this.interval é o ID do intervalo retornado pelo "setInterval()";
+    this.interval = setInterval(() => this.setState(({ timer }) => ({
+      timer: timer - 1,
+    })), oneSecond);
   }
 
   handleChangeStyle() {
@@ -88,13 +115,14 @@ class ActualQuestion extends Component {
       incorrect_answers: incorrectAnswers,
     } } = this.props;
 
-    const { answered } = this.state;
+    const { answered, timer } = this.state;
 
     const answers = [...incorrectAnswers, correctAnswer];
     answers.sort();
 
     return (
       <section>
+        <p>{ timer }</p>
         <h2 data-testid="question-category">{ category }</h2>
         <p data-testid="question-text">{ question }</p>
         <div>
