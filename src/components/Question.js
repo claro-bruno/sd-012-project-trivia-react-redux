68b7842f2ccf35e10c676ef87b20e3bc739031fe
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-08-05 18:34:37 -0300

    Mudando as importações de lugar

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import UniqueButton from './UniqueButton';
-import Button from './Button';
-import './question.css';
 import Timer from './Timer';
 
 class Question extends Component {
--- src/components/QuestionInfo.js
+++ src/components/QuestionInfo.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import UniqueButton from './UniqueButton';
+import Button from './Button';
+import './questioninfo.css';
+
 
 class QuestionInfo extends Component {
   constructor() {
