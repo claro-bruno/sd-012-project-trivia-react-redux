6327786b401b4de22b6d0b7c4e823b869d5ee07f
camila-mp <ca.malvp@gmail.com>
2021-08-05 18:24:12 -0300

    implementa connect no componente Game

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderQuestions = this.renderQuestions.bind(this);
+  }
+
+  renderQuestions() {
+    const { questionsResponse: { questions } } = this.props;
+    questions.map((item) => (
+      <h3 key={ item.correct_answer }>{ item.category }</h3>));
+  }
+
   render() {
     return (
       <section>
         <h3>Question Description</h3>
-        <h3 data-testid="question-category">Category</h3>
+        <h3 data-testid="question-category">{ this.renderQuestions() }</h3>
         <h3 data-testid="question-text">Question</h3>
         <ul>
           <li />
         </ul>
+
       </section>
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => ({
+  questionsResponse: state.questions,
+});
+
+export default connect(mapStateToProps, null)(Game);
