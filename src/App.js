4296ca75b5bb864f423dffdceace1bad6072a387
Bernardo-Coelho-Cezario <80549030+Bernardo-Coelho-Cezario@users.noreply.github.com>
2021-08-05 16:22:17 -0300

    [Group 23] [Bernardo] Requisito 3 (#271)
    * Exercicio 3
    
    * Coloca / no Link
    
    Co-authored-by: Roberval Filho <78314158+rslfilho@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
 import Quiz from './pages/Quiz';
@@ -10,10 +11,11 @@ class App extends React.Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={ Login } />
+        <Route path="/ranking" component={ Ranking } />
         <Route path="/feedback" component={ Feedback } />
-        <Route exact path="/ranking" component={ Ranking } />
         <Route path="/quiz" component={ Quiz } />
+        <Route path="/settings" component={ Settings } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     );
   }
