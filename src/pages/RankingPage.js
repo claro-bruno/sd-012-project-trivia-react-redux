1f270f5184b5bf3bf9e96cf6d489833020799cba
Daniel Batista <62209642+danielbped@users.noreply.github.com>
2021-08-05 15:28:51 -0300

    Main group 19 requisito 16 (#336)
    * Resolve requisito 8
    
    * Finaliza requisito 16
    
    Co-authored-by: adryanrosa <81928512+adryanrosa@users.noreply.github.com>

---
+++ src/pages/RankingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RankingPage extends Component {
+  render() {
+    return (
+      <h1 data-testid="ranking-title">Ranking</h1>
+    );
+  }
+}
+
+export default RankingPage;
