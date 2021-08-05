05f8428d6e6328689fe888ef799ec7432289bc12
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-08-05 20:05:38 -0300

    Corrige erros de lint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
-import Game from './pages/Game';
-import Config from './pages/Config';
-import Login from './pages/Login';
+import Game from './Pages/Game';
+import Config from './Pages/Config';
+import Login from './Pages/Login';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
-        {/* <Route path="/game" component={ Game } /> */}
         <Route path="/game" render={ (props) => <Game { ...props } /> } />
         <Route path="/configs" render={ (props) => <Config { ...props } /> } />
         <Route exact path="/" render={ (props) => <Login { ...props } /> } />
