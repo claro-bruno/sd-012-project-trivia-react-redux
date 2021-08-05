972fb9fca07baa495e0bf6f1f1b6f7a3a6b2f875
Gabriela Guerra <82346552+Gabriela-Guerra@users.noreply.github.com>
2021-08-05 14:27:47 -0300

    [Grupo 10] [Requisito 4] (#239)
    * adiciona requisito 4
    
    * adiciona requisito 4 cypress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,38 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import md5 from 'crypto-js/md5';
+import { connect } from 'react-redux';
+
+const email = md5('email@email.com').toString();
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: '',
-      score: 0,
-      picture: '',
-    };
-  }
-
   render() {
-    const { state: { name, score, picture } } = this;
+    const {
+      props: { name, score } } = this;
     return (
       <header>
-        <img data-testid="header-profile-picture" src={ picture } alt="Player avatar" />
-        <h2 data-testid="header-player-name">{ name }</h2>
-        <aside data-testid="header-score">{ score }</aside>
+        <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${email}` } alt="Player avatar" />
+        <h2 data-testid="header-player-name">{ `User: ${name}` }</h2>
+        <aside data-testid="header-score">{ `Score: ${score}` }</aside>
       </header>
     );
   }
 }
 
-// const {
-//   string,
-//   number,
-// } = PropTypes;
+const {
+  string,
+  number,
+} = PropTypes;
 
-// Header.propTypes = {
-//   name: string.isRequired,
-//   score: number.isRequired,
-//   picture: string.isRequired,
-// };
+Header.propTypes = {
+  name: string.isRequired,
+  score: number.isRequired,
+};
 
-export default Header;
+const mapStateToProps = (state) => ({
+  name: state.user.name,
+  score: state.user.score,
+});
+
+export default connect(mapStateToProps)(Header);
+// !
