21700195616632c9b471ac99afb09b62e05457ad
MGSiqueira <51840153+MGSiqueira@users.noreply.github.com>
2021-08-06 18:47:03 -0300

    [+18][Requisitos 13 a 16] (#461)
    * Requisito 12
    
    * Requisitos 13, 14, 15 e 16
    
    * Requisitos 13 a 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
+import Performance from '../components/Performance';
+
 
 export default class Config extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shouldRedirectLogin: false,
+      shouldRedirectRanking: false,
+    };
+
+    this.redirectLogin = this.redirectLogin.bind(this);
+    this.redirectRanking = this.redirectRanking.bind(this);
+  }
+
+  redirectLogin() {
+    this.setState({ shouldRedirectLogin: true });
+  }
+
+  redirectRanking() {
+    this.setState({ shouldRedirectRanking: true });
+  }
+
   render() {
+    const { shouldRedirectLogin, shouldRedirectRanking } = this.state;
+    if (shouldRedirectLogin) return <Redirect to="/" />;
+    if (shouldRedirectRanking) return <Redirect to="/ranking" />;
     return (
       <div>
         <Header />
         <h1 data-testid="feedback-text">Feedback Bonito</h1>
+        <Performance />
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.redirectLogin }
+        >
+          Jogar novamente
+        </button>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.redirectRanking }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
