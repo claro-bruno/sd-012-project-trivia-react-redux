a206841b46f97dca742fb0a2d0cd937395b801a5
David Azevedo <zevid.dev@outlook.com>
2021-08-06 19:27:24 -0300

    Cria botao para jogar novamente, enviando a pessoa para a login

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Feedback extends React.Component {
   render() {
-    return <h1 data-testid="feedback-text">Feedback</h1>;
+    const { history } = this.props;
+    return (
+      <div>
+        <h1 data-testid="feedback-text">Feedback</h1>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ () => history.push('/') }
+        >
+          Jogar novamente
+        </button>
+      </div>
+    );
   }
 }
+Feedback.propTypes = {
+  history: PropTypes.shape(Object).isRequired,
+};
 
 export default Feedback;
