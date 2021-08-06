8d3629957321fe86233045cbd91a930b162c06ef
Armando <a.guerra58@hotmail.com>
2021-08-06 17:44:28 -0300

    Requisito 18 group 25 [Armando-Guerra]

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  constructor() {
+    super();
+    this.playAgain = this.playAgain.bind(this);
+  }
+
+  playAgain() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
         <p data-testid="ranking-title">
           Ranking
         </p>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.playAgain }
+        >
+          Voltar ao inicio
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.func,
+}.isRequired;
 
 export default Ranking;
