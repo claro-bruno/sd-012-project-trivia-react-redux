66edbd333797c5d0c1bee6efb8ae650aeff2a425
MGSiqueira <marcos.siqueira98@gmail.com>
2021-08-06 05:23:48 -0300

    Deletando branchs, vamos ver se some

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Config extends React.Component {
   render() {
     return (
-      <h1 data-testid="feedback-text">Feedback</h1>
+      <h1 data-testid="feedback-text">Feedback Bonito</h1>
     );
   }
 }
