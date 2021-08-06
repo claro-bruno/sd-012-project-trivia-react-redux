4136f2451e1ed79366c7edbb839682543062edf9
Renan Ferreira <renansantiago2013@hotmail.com>
2021-08-06 16:30:11 -0300

    Resolve requisito 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import md5 from 'crypto-js/md5';
 
 class FeedBack extends React.Component {
   render() {
+    const {
+      player:
+      { name, gravatarEmail, score } } = JSON.parse(localStorage.getItem('state'));
     return (
       <div>
         <p data-testid="feedback-text">FeedBack</p>
+        <img
+          src={ `https://www.gravatar.com/avatar/${md5(gravatarEmail).toString()}` }
+          data-testid="header-profile-picture"
+          alt={ `Name:${name}` }
+        />
+        <h3 data-testid="header-player-name">{name}</h3>
+        <p data-testid="header-score">{score}</p>
       </div>
     );
   }
