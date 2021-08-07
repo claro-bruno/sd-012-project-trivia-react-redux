dcf551f57762934a429f1f0aaaa4d582d1281544
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-07 16:54:30 -0300

    cria e implementa button HOME na page Ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import md5 from 'crypto-js/md5';
+import { Redirect } from 'react-router-dom';
 import arquivoTesteRanking from '../arquivoTesteRanking';
+import Button from '../components/Button';
 
 class Ranking extends React.Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      redirect: false,
+    };
+  }
+
+  handleClick() {
+    this.setState((state) => ({
+      ...state,
+      redirect: true,
+    }));
+  }
+
   render() {
+    const {
+      state: { redirect },
+      handleClick,
+    } = this;
     const TESTEsort = arquivoTesteRanking.sort((a, b) => b.score - a.score);
     console.log(TESTEsort);
 
     return (
       <>
+        { redirect && <Redirect to="/" /> }
         <h2>Ranking</h2>
+        <Button
+          name="HOME"
+          testId="btn-go-home"
+          handleClick={ handleClick }
+        />
         <ol>
           {
             TESTEsort.map((posicao, index) => {
@@ -19,21 +48,14 @@ class Ranking extends React.Component {
 
               return (
                 <li key={ `${score}.${name}.${score}` }>
-                  <img
-                    src={ SRC }
-                    alt={ `Player avatar ${index}` }
-                  />
-                  <h3
-                    data-testid={ `player-name-${index}` }
-                  >
+                  <img src={ SRC } alt={ `Player avatar ${index}` } />
+                  <h3 data-testid={ `player-name-${index}` }>
                     { `NOME: ${name}` }
                   </h3>
                   <h4>
                     { `ACERTOS: ${assertions}` }
                   </h4>
-                  <h4
-                    data-testid={ `player-score-${index}` }
-                  >
+                  <h4 data-testid={ `player-score-${index}` }>
                     { `PONTUAÇÃO: ${score}` }
                   </h4>
                 </li>
