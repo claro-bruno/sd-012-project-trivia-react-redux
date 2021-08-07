8f3755267c0a7a42a4c9a1c822696370602676be
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-07 16:42:24 -0300

    implementa page Ranking

---
+++ src/arquivoTesteRanking.js
@@ -0,0 +1,28 @@
+const arquivoTesteRanking = [
+  {
+    name: 'Wander',
+    assertions: 4,
+    score: 400,
+    gravatarEmail: 'wanveloso@yahoo.com.br',
+  },
+  {
+    name: 'Gabriela',
+    assertions: 5,
+    score: 500,
+    gravatarEmail: 'gabriela@email.com.br',
+  },
+  {
+    name: 'Lary',
+    assertions: 3,
+    score: 300,
+    gravatarEmail: 'lary.com.br',
+  },
+  {
+    name: 'Alvaro',
+    assertions: 2,
+    score: 200,
+    gravatarEmail: 'alvaro@email.com.br',
+  },
+];
+
+export default arquivoTesteRanking;
--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import md5 from 'crypto-js/md5';
+import arquivoTesteRanking from '../arquivoTesteRanking';
 
 class Ranking extends React.Component {
   render() {
-    return 'Tela de Ranking!';
+    const TESTEsort = arquivoTesteRanking.sort((a, b) => b.score - a.score);
+    console.log(TESTEsort);
+
+    return (
+      <>
+        <h2>Ranking</h2>
+        <ol>
+          {
+            TESTEsort.map((posicao, index) => {
+              const { name, assertions, score, gravatarEmail } = posicao;
+              const profile = md5(gravatarEmail).toString();
+              const SRC = `https://www.gravatar.com/avatar/${profile}`;
+
+              return (
+                <li key={ `${score}.${name}.${score}` }>
+                  <img
+                    src={ SRC }
+                    alt={ `Player avatar ${index}` }
+                  />
+                  <h3
+                    data-testid={ `player-name-${index}` }
+                  >
+                    { `NOME: ${name}` }
+                  </h3>
+                  <h4>
+                    { `ACERTOS: ${assertions}` }
+                  </h4>
+                  <h4
+                    data-testid={ `player-score-${index}` }
+                  >
+                    { `PONTUAÇÃO: ${score}` }
+                  </h4>
+                </li>
+              );
+            })
+          }
+        </ol>
+      </>
+    );
   }
 }
 
