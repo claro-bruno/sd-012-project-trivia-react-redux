efed05ea175ead6eba428fe61aebbd4b214d36a6
Luiz Henrique <82241841+lzzhenrique@users.noreply.github.com>
2021-08-07 17:22:32 -0300

    [Grupo 36] [REQ 14 & 15] (#494)
    * Estrutura App
    
    * Adiciona desafio 1
    
    * Refatora REQ 8
    
    * Adiciona requisito 9 e 11
    
    * Tentativa de resolver conflito 1
    
    * Tentativa de resolver conflito 2
    
    * corrige lint
    
    * Tentativa 03 de resolver conflitos
    
    * testando hipotese de erro na chave asserts
    
    * Chave assert passa a ser acrescida apos resposta correta
    
    * FIX Req 12
    
    * Adiciona REQ 13
    
    * Adiciona requisito 14 e 15
    
    * corrige lint

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -7,15 +7,17 @@ import Header from '../components/Header';
 class Feedback extends React.Component {
   render() {
     const controlNumber = 3;
-    const { playerAsserts } = this.props;
+    const { playerAsserts, playerScore } = this.props;
     return (
       <div>
+        <Header />
         <p data-testid="feedback-text">
           {
             playerAsserts < controlNumber ? 'Podia ser melhor...' : 'Mandou bem!'
           }
         </p>
-        <Header />
+        <p data-testid="feedback-total-score">{playerScore}</p>
+        <p data-testid="feedback-total-question">{playerAsserts}</p>
         <Link to="/ranking">
           <button
             data-testid="btn-ranking"
@@ -24,12 +26,21 @@ class Feedback extends React.Component {
             Ver Ranking
           </button>
         </Link>
+        <Link to="/">
+          <button
+            data-testid="btn-play-again"
+            type="button"
+          >
+            Jogar novamente
+          </button>
+        </Link>
       </div>);
   }
 }
 
 const mapStateToProps = (state) => ({
   playerAsserts: state.score.asserts,
+  playerScore: state.score.score,
 });
 
 export default connect(mapStateToProps)(Feedback);
