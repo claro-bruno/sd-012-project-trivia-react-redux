19bc2ddeeeb1534337f1af3c0b8194fcd99ecb0a
Lucas Basques <lukasbasques@gmail.com>
2021-08-06 21:32:02 -0300

    Requisitos 12, 15 e 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import FinalResults from '../components/FinalResults';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  rankingBtn() {
+    return (
+      <Link to="/Ranking">
+        <button type="button" data-testid="btn-ranking">
+          Ranking
+        </button>
+      </Link>
+    );
+  }
+
+  playAgainBtn() {
+    return (
+      <Link to="/">
+        <button type="button" data-testid="btn-play-again">
+          Jogar novamente
+        </button>
+      </Link>
+    );
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <FinalResults />
+        {this.playAgainBtn()}
+        {this.rankingBtn()}
       </div>
     );
   }
