b0f3a861977ef574726ee862da7c378cadee2d5d
Patrick Dack <patrickdack89@gmail.com>
2021-08-08 01:32:24 -0300

    Requisito 11 implementado

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './component/Login';
 import Config from './component/Config';
 import Quest from './component/Quest';
+import Feedback from './pages/Feedback';
 
 function Routes() {
   return (
     <Switch>
       <Route path="/quest" component={ Quest } />
       <Route path="/config" component={ Config } />
+      <Route path="/feedback" component={ Feedback } />
       <Route path="/" component={ Login } />
     </Switch>
   );
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router';
-// import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import logo from '../trivia.png';
 import * as fetchAPI from '../helpers/fetchAPI';
