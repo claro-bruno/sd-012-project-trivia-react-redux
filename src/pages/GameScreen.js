fab17c393339113652e5797a7b2fd3864719e4f5
Wadson Almeida <wadson.dev92@gmail.com>
2021-08-08 18:48:53 -0300

    requisito 9

--- src/pages/GameScreen.js
+++ src/pages/GameScreen.js
@@ -11,22 +11,72 @@ class GameScreen extends Component {
       count: 0,
       borderGreen: 'without',
       borderRed: 'without',
+      question: 'correct',
+      score: 0,
+      assertions: 0,
+      name: '',
+      gravatarEmail: '',
     };
 
     this.renderHeader = this.renderHeader.bind(this);
     this.renderQuestionsApi = this.renderQuestionsApi.bind(this);
-    this.changeBorderAnswerClick = this.changeBorderAnswerClick.bind(this);
+    this.handleAnswerButtonClick = this.handleAnswerButtonClick.bind(this);
+    this.getPointsAndSaveInLocalStorage = this.getPointsAndSaveInLocalStorage.bind(this);
   }
 
-  changeBorderAnswerClick() {
+  componentDidUpdate() {
+    const { score, assertions, name, gravatarEmail } = this.state;
+
+    const stateLocalStorage = {
+      player: {
+        score,
+        assertions,
+        name,
+        gravatarEmail,
+      },
+    };
+    const keyLocalStorage = JSON.stringify(stateLocalStorage);
+    localStorage.setItem('state', keyLocalStorage);
+  }
+
+  getPointsAndSaveInLocalStorage() {
+    const { score, assertions, question } = this.state;
+    const point = 10;
+    const dificulty = {
+      hard: 3,
+      medium: 2,
+      easy: 1,
+    };
+    if (question === 'correct' && dificulty.hard) {
+      this.setState({
+        score: (score + point + dificulty.hard),
+        assertions: (assertions + 1),
+      });
+    } else if (question === 'correct' && dificulty.medium) {
+      this.setState({
+        score: (score + point + dificulty.medium),
+        assertions: (assertions + 1),
+      });
+    } else if (question === 'correct' && dificulty.easy) {
+      this.setState({
+        score: (score + point + dificulty.easy),
+        assertions: (assertions + 1),
+      });
+    }
+    // return score;
+  }
+
+  handleAnswerButtonClick(click) {
     this.setState({
       borderGreen: 'border-green',
       borderRed: 'border-red',
     });
+    this.getPointsAndSaveInLocalStorage(click);
   }
 
   renderHeader() {
     const { userPlayer: { name, gravatarEmail } } = this.props;
+    const { score } = this.state;
     return (
       <header>
         <img
@@ -35,14 +85,14 @@ class GameScreen extends Component {
           data-testid="header-profile-picture"
         />
         <h2 data-testid="header-player-name">{ name }</h2>
-        <h3 data-testid="header-score">0</h3>
+        <h3 data-testid="header-score">{ score }</h3>
       </header>
     );
   }
 
   renderQuestionsApi() {
     const { requestGameApi } = this.props;
-    const { count } = this.state;
+    const { count, question } = this.state;
     const dataResults = requestGameApi.results;
     const incorrectAnswers = dataResults && dataResults
       .map((item) => item.incorrect_answers)[count];
@@ -58,9 +108,10 @@ class GameScreen extends Component {
             <p data-testid="question-text">{ item.question }</p>
             <button
               type="button"
+              id={ question }
               data-testid="correct-answer"
               className={ borderGreen }
-              onClick={ () => this.changeBorderAnswerClick() }
+              onClick={ () => this.handleAnswerButtonClick() }
             >
               { item.correct_answer }
             </button>
@@ -69,10 +120,11 @@ class GameScreen extends Component {
         { incorrectAnswers && incorrectAnswers.map((item, index) => (
           <button
             type="button"
+            name="incorrect"
             data-testid={ `wrong-answer-${index}` }
             key={ index }
             className={ borderRed }
-            onClick={ () => this.changeBorderAnswerClick() }
+            onClick={ () => this.handleAnswerButtonClick() }
           >
             { item }
           </button>
