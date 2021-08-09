0cbe25e4861c621b510e1e84d95ea8c3b975dbc1
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-08-09 12:07:34 -0300

    corrige problema com styles:lint

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import './Loading.css';
 
 class Loading extends React.Component {
   render() {
     return (
-      <div className="loadingio-spinner-pulse-s0vib6lgbal">
-        <div className="ldio-mxjbkfncw9">
-          <div />
-          <div />
-          <div />
-        </div>
+      <div className="loading-content">
+        <h3>Loading...</h3>
       </div>
     );
   }
