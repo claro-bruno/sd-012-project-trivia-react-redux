c25aaed096989d9314f6fc293aa4f18c0113af04
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-08-09 20:18:46 -0300

    req 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
     return (
-      <p data-testid="ranking-title">Ranking</p>
+      <>
+        <p data-testid="ranking-title">Ranking</p>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Voltar ao início
+          </button>
+        </Link>
+      </>
     );
   }
 }
