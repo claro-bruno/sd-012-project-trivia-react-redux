3e7fc8e62c8914421b37effe0300cb5bb54bad90
Adryan Rosa <adryanrosa374@gmail.com>
2021-08-09 16:06:29 -0300

    Adiciona rankings

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -3,9 +3,22 @@ import { Link } from 'react-router-dom';
 
 class RankingPage extends Component {
   render() {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+
     return (
       <main>
         <h1 data-testid="ranking-title">Ranking</h1>
+
+        {ranking
+          .sort((a, b) => b.score - a.score)
+          .map(({ name, score, picture }, index) => (
+            <div key={ index }>
+              <p data-testid={ `player-name-${index}` }>{name}</p>
+              <p data-testid={ `player-score-${index}` }>{score}</p>
+              <img src={ picture } alt={ name } />
+            </div>
+          ))}
+
         <Link to="/">
           <button
             type="button"
