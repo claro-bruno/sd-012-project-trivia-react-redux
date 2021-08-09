a5fd9dcbeda80341c9ae833402c91a89189fd6bf
Alice Gonçalves <galice9992@gmail.com>
2021-08-09 16:23:01 -0300

    requisito 14 implementado

---
+++ src/component/FeedbackInfo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class FeedbackInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    const { player: { score, assertions } } = JSON.parse(localStorage.getItem('state'));
+    const assertionsNumber = parseInt(assertions, 10);
+    const scoreNumber = parseInt(score, 10);
+    this.state = {
+      assertions: assertionsNumber,
+      score: scoreNumber,
+    };
+  }
+
+  render() {
+    const { assertions, score } = this.state;
+    return (
+      <div>
+        <h3 data-testid="feedback-total-score">{ score }</h3>
+        <h3 data-testid="feedback-total-question">{ assertions }</h3>
+      </div>
+    );
+  }
+}
+
+export default FeedbackInfo;
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FeedbackText from '../component/FeedbackText';
+import FeedbackInfo from '../component/FeedbackInfo';
 
 class Feedback extends React.Component {
   render() {
     return (
       <div>
         <FeedbackText />
+        <FeedbackInfo />
       </div>
     );
   }
