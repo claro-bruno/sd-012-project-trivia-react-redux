8d2a3c1e893e6717c8c210e77c65b8a35256a8ce
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-08-09 19:23:08 -0300

    alteraçoes req 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Game from './Game';
 
@@ -37,6 +38,14 @@ class Feedback extends React.Component {
           </span>
         </p>
         <p data-testid="feedback-text">{ this.feedbackText() }</p>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-play-again"
+          >
+            Jogar novamente
+          </button>
+        </Link>
       </>
     );
   }
