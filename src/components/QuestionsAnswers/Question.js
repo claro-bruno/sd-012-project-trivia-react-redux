7cd75020113af5ebdeee2edf084f8c2e2d753c77
Ana Laura <analauraalves25@gmail.com>
2021-08-09 14:41:59 -0300

    resolve problemas do lint - requisito 5

--- src/components/QuestionsAnswers/Question.js
+++ src/components/QuestionsAnswers/Question.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Question extends React.Component {
   constructor() {
@@ -6,59 +7,53 @@ class Question extends React.Component {
 
     this.state = {
       index: 0,
-      isCorrect: false,
     };
 
     this.handleNext = this.handleNext.bind(this);
-    this.handleCorrectAnswer = this.handleCorrectAnswer.bind(this);
   }
 
   handleNext() {
     this.setState((state) => ({
       index: state.index + 1,
-    }))
-  }
-
-  handleCorrectAnswer(event) {
-    if (event.target.classList.contains('correct')) {
-      this.setState({
-        isCorrect: true,
-      })
-    }
+    }));
   }
 
   render() {
     const { index } = this.state;
     const { questions } = this.props;
     return (
-      <section>    
-        {!questions.length ? null : 
-        <>  
-          <p data-testid="question-category">{ questions[index].category }</p>
-          <h3 data-testid="question-text">{ questions[index].question }</h3>
-          <button
-            type="button"
-            onClick={ (event) => this.handleCorrectAnswer(event) }
-            data-testid="correct-answer"
-            className="correct"
-          >
-          {questions[index].correct_answer}
-        </button>
-        {questions[index].incorrect_answers.map((wrongAnswer, index) => (
-          <button
-            type="button"
-            onClick={ (event) => this.handleCorrectAnswer(event) }
-            data-testid={ `wrong-answer-${index}` }
-          >
-            {wrongAnswer}
-          </button>
-        ))}
-          <button type="button" onClick={ this.handleNext }>NEXT QUESTION</button>
-        </>
-        }
+      <section>
+        {!questions.length ? null : (
+          <div>
+            <p data-testid="question-category">{ questions[index].category }</p>
+            <h3 data-testid="question-text">{ questions[index].question }</h3>
+            <button
+              type="button"
+              data-testid="correct-answer"
+              className="correct"
+            >
+              {questions[index].correct_answer}
+            </button>
+            {questions[index].incorrect_answers.map((wrongAnswer, indexIncorrect) => (
+              <button
+                type="button"
+                onClick={ (event) => this.handleCorrectAnswer(event) }
+                data-testid={ `wrong-answer-${indexIncorrect}` }
+                key={ indexIncorrect }
+              >
+                {wrongAnswer}
+              </button>
+            ))}
+            <button type="button" onClick={ this.handleNext }>NEXT QUESTION</button>
+          </div>
+        )}
       </section>
     );
   }
 }
+
+Question.propTypes = {
+  questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default Question;
