bbd956788dd3f40c9abbdf31130a116b926c61b2
Sass <dudu_sass12@hotmail.com>
2021-08-09 15:54:42 -0300

    req 15 e 16

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Feedback extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: false,
+    };
+    this.changeRoute = this.changeRoute.bind(this);
+  }
+
+  changeRoute() {
+    this.setState({ redirect: true });
+  }
+
   render() {
+    const { redirect } = this.state;
     return (
-      <h1 data-testid="feedback-text">Feedbacks</h1>
+      <div>
+        <h1 data-testid="feedback-text">Feedbacks</h1>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.changeRoute }
+        >
+          Jogar Novamente
+        </button>
+        { redirect && (<Redirect to="/" />) }
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.changeRoute}
+        >
+          Ver Ranking
+          </button> 
+          { redirect && (<Redirect to="ranking" />) }
+      </div>
     );
   }
 }
