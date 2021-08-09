6cf208cd241878199bf58baef24b4a86c3b4fa95
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-08-09 10:51:10 -0300

    R10| Adiciona botão próxima pergunta

---
+++ src/Components/NextButton.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { nextQuestion } from '../redux/actions';
+
+class NextButton extends Component {
+  render() {
+    const { dispatchNextQuestion, isLastQuestion, history } = this.props;
+    return (
+      <button
+        data-testid="btn-next"
+        type="button"
+        onClick={ isLastQuestion ? () => history.push('/results') : dispatchNextQuestion }
+      >
+        Next
+      </button>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLastQuestion: state.game.isLastQuestion,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchNextQuestion: () => dispatch(nextQuestion()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NextButton);
+
+NextButton.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatchNextQuestion: PropTypes.func.isRequired,
+  isLastQuestion: PropTypes.bool.isRequired,
+};
