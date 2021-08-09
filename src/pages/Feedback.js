b4aeceb6433e12a94ce5431a4f0f45cff0840569
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-08-09 18:43:30 -0300

    req 13 - em partes - aguarda req 9 para conclusao e adaptaçao do redux

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,12 +1,48 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Game from './Game';
 
 class Feedback extends React.Component {
+  // constructor(){
+  //  super();
+  //  this.feedbackText = this.feedbackText.bind(this);
+  // }
+
+  feedbackText() {
+    const { correctAnswers } = this.props;
+    const parametroResposta = 3;
+    if (correctAnswers < parametroResposta) {
+      return 'Podia ser melhor...';
+    }
+    return 'Mandou bem!';
+  }
+
   render() {
+    const { correctAnswers } = this.props;
     return (
-      <Game />
+      <>
+        <Game />
+        <p>
+          Acertos:
+          <span>
+            { correctAnswers }
+          </span>
+        </p>
+        <p data-testid="feedback-text">{ this.feedbackText() }</p>
+      </>
     );
   }
 }
 
+Feedback.propTypes = {
+  correctAnswers: PropTypes.number.isRequired,
+};
+
+/*  const mapStateToProps = (state) => ({
+  correctAnswers: state.user.assertions,
+}); */
+
 export default Feedback;
+
+// export default connect(mapStateToProps)(Feedback);
