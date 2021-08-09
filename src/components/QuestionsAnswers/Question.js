4d727a637fef1e03eeaf1608213fd62696750de4
Ana Laura <analauraalves25@gmail.com>
2021-08-09 18:35:33 -0300

    refatora requisito 5

--- src/components/QuestionsAnswers/Question.js
+++ src/components/QuestionsAnswers/Question.js
@@ -8,44 +8,78 @@ class Question extends React.Component {
 
     this.state = {
       index: 0,
+      isCorrect: '',
+      answered: false,
     };
 
     this.handleNext = this.handleNext.bind(this);
+    this.handleCorrectAnswer = this.handleCorrectAnswer.bind(this);
+    this.startQuiz = this.startQuiz.bind(this);
+  }
+
+  componentDidMount() {
+    this.startQuiz();
+  }
+
+  handleCorrectAnswer() {
+    this.setState({
+      answered: true,
+    });
+  }
+
+  startQuiz() {
+    const { questions } = this.props;
+    const { index, isCorrect } = this.state;
+    this.setState({
+      isCorrect: questions[index].correct_answer,
+    });
+    console.log(isCorrect);
   }
 
   handleNext() {
     this.setState((state) => ({
       index: state.index + 1,
+      answered: false,
     }));
   }
 
   render() {
-    const { index } = this.state;
+    const { index, answered, isCorrect } = this.state;
     const { questions } = this.props;
+    const lastQuestion = questions.length === index + 1;
+    const { incorrect_answers: incorrectAnswers,
+      correct_answer: correctAnswer } = questions[index];
+    const NUMBER = 0.5;
+    const newArray = [...incorrectAnswers, correctAnswer]
+      .sort(() => (
+        Math.random() - NUMBER
+      ));
+    console.log(newArray);
     return (
       <section>
         {!questions.length ? null : (
           <div>
             <p data-testid="question-category">{ questions[index].category }</p>
             <h3 data-testid="question-text">{ questions[index].question }</h3>
-            <button
-              type="button"
-              data-testid="correct-answer"
-              className="correct"
-            >
-              {questions[index].correct_answer}
-            </button>
-            {questions[index].incorrect_answers.map((wrongAnswer, indexIncorrect) => (
+            {newArray.map((option, indice) => (
               <button
                 type="button"
-                onClick={ (event) => this.handleCorrectAnswer(event) }
-                data-testid={ `wrong-answer-${indexIncorrect}` }
-                key={ indexIncorrect }
+                key={ indice }
+                onClick={ () => this.handleCorrectAnswer() }
+                disabled={ answered }
+                data-testid={ isCorrect.includes(option)
+                  ? 'correct-answer' : `wrong-answer-${indice}` }
               >
-                {wrongAnswer}
+                {option}
               </button>
             ))}
-            <button type="button" onClick={ this.handleNext }>NEXT QUESTION</button>
+            {answered && (
+              <button
+                type="button"
+                onClick={ lastQuestion ? 'acabou' : this.handleNext }
+              >
+                PRÓXIMA
+              </button>)}
           </div>
         )}
       </section>
