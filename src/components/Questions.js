1d5ecfb2abd2446cb1643d279c5d75fb269cf683
Bernardo-Coelho-Cezario <80549030+Bernardo-Coelho-Cezario@users.noreply.github.com>
2021-08-10 14:14:40 -0300

    [Grupo 23] [Bernardo] - Requisito 11

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/Questions.css';
+import { Redirect } from 'react-router-dom';
 
 const MINUS_ONE = -1;
 
@@ -11,11 +12,14 @@ class Questions extends Component {
       disabled: false,
       next: false,
       time: 30,
+      redirect: false,
     };
     this.getUnities = this.getUnities.bind(this);
     this.answersRender = this.answersRender.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.countdown = this.countdown.bind(this);
+    this.buttonFunction = this.buttonFunction.bind(this);
+    this.removeFirstQuestion = this.removeFirstQuestion.bind(this);
   }
 
   componentDidMount() {
@@ -35,8 +39,7 @@ class Questions extends Component {
   }
 
   handleClick({ target }) {
-    this.setState({ disabled: true, next: true });
-    target.classList.add('selected');
+    this.setState({ disabled: true, next: true }, () => target.classList.add('selected'));
     clearInterval(this.interval);
   }
 
@@ -107,15 +110,48 @@ class Questions extends Component {
     }, THIRTY_SECONDS);
   }
 
+  removeFirstQuestion() {
+    const { questions } = this.state;
+    const meuArray = [...questions];
+
+    if (meuArray.length > 1) {
+      meuArray.shift();
+      this.setState({
+        next: false,
+        questions: meuArray,
+        disabled: false,
+        time: 30,
+      }, () => {
+        this.countdown();
+      });
+    } else {
+      this.setState({ redirect: true });
+    }
+  }
+
+  buttonFunction() {
+    return (
+      <button
+        data-testid="btn-next"
+        type="button"
+        onClick={ () => this.removeFirstQuestion() }
+      >
+        Proxima Pergunta
+      </button>);
+  }
+
   render() {
-    const { questions, next, time } = this.state;
+    const { questions, next, time, redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/feedback" />;
+    }
     return (
       <main>
         <div className="timer">
           {time}
         </div>
         {questions[0] && this.answersRender()}
-        {next && <button data-testid="btn-next" type="button">Proxima Pergunta</button>}
+        {next ? this.buttonFunction() : null}
       </main>
     );
   }
