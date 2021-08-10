b2cfe5effa38bb29090f1f389a5014678fd95701
RenanMiyashita <renanmiyashita@gmail.com>
2021-08-10 10:33:19 -0300

    grupo-35-requisitos-13-14

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 import UserProfile from '../components/UserProfile';
 
 export default class extends Component {
+  constructor() {
+    super();
+    this.alertScore = this.alertScore.bind(this);
+  }
+
+  alertScore() {
+    const getAssertion = JSON.parse(localStorage.getItem('state'));
+    const { player } = getAssertion;
+    const { assertions } = player;
+    const number = 3;
+
+    if (assertions < number) {
+      return ('Podia ser melhor...');
+    }
+    return ('Mandou bem!');
+  }
+
   render() {
+    const getAssertion = JSON.parse(localStorage.getItem('state'));
+    const { player } = getAssertion;
+
     return (
       <div>
         <UserProfile />
-        <p data-testid="feedback-text">Feedback</p>
+        <h1 data-testid="feedback-text">{ this.alertScore() }</h1>
+        <h2 data-testid="feedback-total-score">
+          { player.score }
+          {' '}
+          pontos
+        </h2>
+        <h3 data-testid="feedback-total-question">
+          { player.assertions }
+          {' '}
+          acerto(s)
+        </h3>
       </div>
     );
   }
