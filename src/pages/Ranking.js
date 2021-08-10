ca91833bc92bc0e7c5f52533c692671e94aeb95e
Victor <juanvictor70@gmail.com>
2021-08-10 13:49:24 -0400

    trabalhando no css v-1.2 e baixando atualizacao da branch

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../Style/ranking.css';
 
 class Ranking extends Component {
   render() {
     const getRanking = JSON.parse(localStorage.getItem('ranking'));
     return (
-      <div>
-        <h2 data-testid="ranking-title">Ranking</h2>
+      <div className="ranking-screen">
+        <h2 id="ranking-title" data-testid="ranking-title">Ranking</h2>
         {
           getRanking.map((user, index) => (
             <div key={ index }>
-              <img src={ user.picture } alt="user icon" />
-              <p data-testid={ `player-name-${index}` }>{ user.name }</p>
-              <p data-testid={ `player-score-${index}` }>{ user.score }</p>
+              <div id="ranking-player">
+                <img id="ranking-img" src={ user.picture } alt="user icon" />
+                <p
+                  id="ranking-name"
+                  data-testid={ `player-name-${index}` }
+                >
+                  { user.name }
+                </p>
+                <p
+                  id="ranking-score"
+                  data-testid={ `player-score-${index}` }
+                >
+                  { user.score }
+                </p>
+              </div>
             </div>
           ))
         }
         <Link to="/">
-          <button type="button" data-testid="btn-go-home">Home!</button>
+          <button id="btn-go-home" type="button" data-testid="btn-go-home">Home</button>
         </Link>
       </div>
     );
