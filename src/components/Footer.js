164bf8e32ddcbd674dc094da7cc2fa14dc4d69a7
Roberval Filho <rslfilho@gmail.com>
2021-08-10 19:00:06 -0300

    Estilização de Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import '../styles/Footer.css';
 
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">
+      <div
+        className="footer
+        bg-header text-center py-3 absolute bottom-0 w-screen text-white"
+      >
         <p>
           <span>Desenvolvido por </span>
-          <a href="https://www.linkedin.com/in/camila-malvessi-6b3367110/" target="_blank" rel="noopener noreferrer"> Bernardo, </a>
-          <a href="https://www.linkedin.com/in/felipelouzeiro/" target="_blank" rel="noopener noreferrer"> Caio, </a>
-          <a href="https://www.linkedin.com/in/guilherme-hermenegildo-junior/" target="_blank" rel="noopener noreferrer"> Guilherme, </a>
-          <a href="https://caiomorato.github.io/portfolio/" target="_blank" rel="noopener noreferrer"> Nuwanda e </a>
-          <a href="https://www.rslfilho.com.br/" target="_blank" rel="noopener noreferrer"> Roberval</a>
+          <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+            Bernardo
+          </a>
+          ,
+          {' '}
+          <a href="https://www.google.com/" target="_blank" rel="noopener noreferrer">
+            Caio
+          </a>
+          ,
+          {' '}
+          <a href="https://www.linkedin.com/in/guilherme-hermenegildo-junior/" target="_blank" rel="noopener noreferrer">
+            Guilherme
+          </a>
+          ,
+          {' '}
+          <a href="https://caiomorato.github.io/portfolio/" target="_blank" rel="noopener noreferrer">
+            Nuwanda
+          </a>
+          {' '}
+          e
+          {' '}
+          <a href="https://www.rslfilho.com.br/" target="_blank" rel="noopener noreferrer">
+            Roberval
+          </a>
           <span> usando React.js e Tailwind CSS - Bloco 17 - </span>
           <a href="http://www.betrybe.com.br/" target="_blank" rel="noopener noreferrer">Trybe</a>
         </p>
