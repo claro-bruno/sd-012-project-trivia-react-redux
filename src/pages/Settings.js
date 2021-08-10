a5e64d5a72ee5942929f2dc23232e87268a197f3
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-08-10 17:45:50 -0300

    adicionando selects a Settings e fazendo requisicao das categorias

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,11 +1,44 @@
 import React from 'react';
 
+const URL = 'https://opentdb.com/api_category.php';
+
 class Settings extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    }
+  }
+  componentDidMount() {
+    fetch(URL)
+    .then((response) => response.json())
+    .then((data) => this.setState({ categories: data.trivia_categories }))
+    .catch((error) => error);
+  }
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <h1 data-testid="settings-title">Configurações</h1>
-        <select></select>
+        <select id="category">
+          {categories.map((category) => ( 
+            <option key={ category.id } value={ category.id }>
+              { category.name }
+            </option>
+          ))}
+        </select>
+        <select id="dificulty">
+          <option value="">Qualquer Dificuldade</option>
+          <option value="easy">Fácil</option>
+          <option value="medium">Médio</option>
+          <option value="hard">Difícil</option>
+        </select>
+        <select id="type">
+          <option value="">Qualquer Tipo</option>
+          <option value="multiple">Multipla Escolha</option>
+          <option value="boolean">Verdadeiro ou Falso</option>
+        </select>
       </div>
     );
   }
