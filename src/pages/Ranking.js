d1bc8eb9808799530d8a0c962603f1818558feb1
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-08-10 13:07:10 -0300

    altera ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 class Ranking extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <p data-testid="ranking-title">Ranking</p>
         <Link to="/">
           <button
