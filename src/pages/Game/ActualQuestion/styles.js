53b4f5389da8c18ebed30cb7432fce0d571f2760
Eric Kreis <erickreis793@gmail.com>
2021-08-10 19:57:28 -0300

    [ESTILIZANDO - Resolvendo problema de indentaçao entre eslint e stylelint]

--- src/pages/Game/ActualQuestion/styles.js
+++ src/pages/Game/ActualQuestion/styles.js
@@ -1,18 +1,19 @@
-/* eslint-disable indent */
 import styled from 'styled-components';
 
 // Isto é um componente estilizado do tipo botão;
 const AnswerButtonS = styled.button` /* desabilitando problema stylelint */
   background-color: transparent;
+
+/* stylelint-disable indentation */
   border: ${({ styles }) => {
     if (styles.answered) {
-    if (styles.correct) {
-    return '3px solid rgb(6, 240, 15)';
-    }
-    return '3px solid rgb(255, 0, 0)';
+      if (styles.correct) {
+        return '3px solid rgb(6, 240, 15)';
+      }
+      return '3px solid rgb(255, 0, 0)';
     }
     return '3px solid black';
-    }};
+  }};
 
   :hover {
     cursor: pointer;
