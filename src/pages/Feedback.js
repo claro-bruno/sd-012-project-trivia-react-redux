ee8562eb719bea9f1886fe6993f6bed34cd186d5
Luan Alexandre <78628115+LuanAlexandre@users.noreply.github.com>
2021-08-09 20:43:15 -0400

    [MAIN-GROUP-36][REQ-17] (#606)
    * solucao do requisito 17
    
    * fix css lint

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -2,15 +2,65 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  componentDidMount() {
+    this.setRankingInLocalStorage(this.getPlayerSpecs());
+  }
+
+  // função que retorna as informações do jogador que estão salvas no localStorage
+  getPlayerSpecs() {
+    const { player } = JSON.parse(localStorage.getItem('state'));
+    return player;
+  }
+
+  // função que retorna a lista de jogadores guardada no localStorage
+  getLocalStorageRanking() {
+    const rankingKey = JSON.parse(localStorage.getItem('ranking'));
+    return rankingKey;
+  }
+
+  // função que salva a lista de jogadores no localStorage
+  setRankingInLocalStorage({ picture, name, score }) {
+    const newPlayer = {
+      name,
+      score,
+      picture,
+    };
+
+    const ranking = this.getLocalStorageRanking();
+    const isRanking = this.verifyLocalStorageRanking(ranking);
+
+    if (!isRanking) {
+      localStorage.setItem('ranking', JSON.stringify([newPlayer]));
+    } else {
+      ranking.push(newPlayer);
+      localStorage.setItem('ranking', JSON.stringify(ranking));
+    }
+  }
+
+  // função que verifica se o existe a chave ranking no localStorage
+  verifyLocalStorageRanking(storageRanking) {
+    if (!storageRanking) return false;
+    return true;
+  }
+
+  renderPlayerSpecs({ picture, name, score }) {
+    return (
+      <header>
+        <img data-testid="header-profile-picture" src={ picture } alt={ name } />
+        <p data-testid="header-player-name">{ name }</p>
+        <p data-testid="header-score">{ score }</p>
+      </header>
+    );
+  }
+
   render() {
     const controlNumber = 3;
     const { playerAsserts, playerScore } = this.props;
     return (
       <div>
-        <Header />
+        {this.renderPlayerSpecs(this.getPlayerSpecs())}
         <p data-testid="feedback-text">
           {
             playerAsserts < controlNumber ? 'Podia ser melhor...' : 'Mandou bem!'
