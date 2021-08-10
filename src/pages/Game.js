ed5347a7af4bf3d184b05e2699f21bcefe20597d
camila-mp <ca.malvp@gmail.com>
2021-08-09 21:01:47 -0300

    requisito 4: cria Header com nome do jogador, foto e placar

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
+import Header from '../components/Header';
 import './Game.css';
 
 class Game extends React.Component {
@@ -93,6 +94,7 @@ class Game extends React.Component {
     const firstQuestion = triviaQuest[0];
     return (
       <section>
+        <Header />
         { firstQuestion ? this.renderQuestions() : <p>LOADING</p> }
       </section>
     );
