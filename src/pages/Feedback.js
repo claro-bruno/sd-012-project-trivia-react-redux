c1e229316a85ff90ffa03b9ff3f83321f17ec6bc
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-08-09 22:20:45 -0300

    Resolve requisito 15, redireciona para pagina de login

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
@@ -11,16 +12,32 @@ class Feedback extends React.Component {
     const finalScore = JSON.parse(pullScore).player.score;
     this.state = {
       score: finalScore,
+      redirect: false,
     };
+    this.redirectLogin = this.redirectLogin.bind(this);
+  }
+
+  redirectLogin() {
+    this.setState({ redirect: true });
   }
 
   render() {
-    const { score } = this.state;
+    const { score, redirect } = this.state;
     const { getUrl, getName } = this.props;
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
     return (
       <>
         <Header getUrl={ getUrl } getName={ getName } score={ score } />
         <p data-testid="feedback-text">Placeholder para requisito 11</p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.redirectLogin }
+        >
+          Jogar Novamente
+        </button>
       </>
     );
   }
