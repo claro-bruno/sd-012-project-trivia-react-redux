39e317cafb9caee99a0b6d72567d1f8a22a39c83
Roberval Filho <78314158+rslfilho@users.noreply.github.com>
2021-08-10 14:34:05 -0300

    [Grupo 23] [Roberval] - Requisito 9 (#625)
    * Adaptando handleClick para selecionar a resposta clicada
    
    * Criando função de calculo de pontos
    
    * Criando action de resposta correta
    
    * Criando reducer de resposta correta
    
    * Concluindo salvando informações no Redux e LocalStorage
    
    * Diminuindo linhas de Questions

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/Questions.css';
+import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { actionCorrectAnswer } from '../redux/actions';
 
 const MINUS_ONE = -1;
+const NUMBER_THREE = 3;
+const NUMBER_TEN = 10;
 
 class Questions extends Component {
   constructor() {
@@ -18,6 +23,11 @@ class Questions extends Component {
     this.answersRender = this.answersRender.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.countdown = this.countdown.bind(this);
+    this.calculateScore = this.calculateScore.bind(this);
+    this.getDifficultyPoints = this.getDifficultyPoints.bind(this);
+    this.setInitialLocalStorage = this.setInitialLocalStorage.bind(this);
+    this.saveScoreRedux = this.saveScoreRedux.bind(this);
+    this.saveScoreLocalStorage = this.saveScoreLocalStorage.bind(this);
     this.buttonFunction = this.buttonFunction.bind(this);
     this.removeFirstQuestion = this.removeFirstQuestion.bind(this);
   }
@@ -25,6 +35,20 @@ class Questions extends Component {
   componentDidMount() {
     this.getUnities();
     this.countdown();
+    this.setInitialLocalStorage();
+  }
+
+  setInitialLocalStorage() {
+    const { score, assertions, name, gravatarEmail } = this.props;
+    const storedInfo = {
+      player: {
+        name,
+        assertions,
+        score,
+        gravatarEmail,
+      },
+    };
+    localStorage.setItem('state', JSON.stringify(storedInfo));
   }
 
   async getUnities() {
@@ -32,15 +56,58 @@ class Questions extends Component {
     const API_URL = `https://opentdb.com/api.php?amount=5&token${token}`;
     const response = await fetch(API_URL);
     const questions = await response.json();
-
     this.setState(() => ({
       questions: questions.results,
     }));
   }
 
+  getDifficultyPoints() {
+    const { questions } = this.state;
+    switch (questions[0].difficulty) {
+    case 'easy':
+      return 1;
+    case 'medium':
+      return 2;
+    case 'hard':
+      return NUMBER_THREE;
+    default:
+      return 1;
+    }
+  }
+
   handleClick({ target }) {
     this.setState({ disabled: true, next: true }, () => target.classList.add('selected'));
     clearInterval(this.interval);
+    this.saveScoreRedux(target);
+  }
+
+  calculateScore(target) {
+    const { time } = this.state;
+    const difficulty = this.getDifficultyPoints();
+    if (target.id === 'correct-answer') {
+      return NUMBER_TEN * (time * difficulty);
+    }
+    return 0;
+  }
+
+  saveScoreRedux(target) {
+    const { correctAnswer } = this.props;
+    const score = this.calculateScore(target);
+    correctAnswer(score);
+    this.saveScoreLocalStorage(score);
+  }
+
+  saveScoreLocalStorage(points) {
+    const { score, assertions, name, gravatarEmail } = this.props;
+    const storedInfo = {
+      player: {
+        name,
+        assertions: assertions + 1,
+        score: score + points,
+        gravatarEmail,
+      },
+    };
+    localStorage.setItem('state', JSON.stringify(storedInfo));
   }
 
   answersRender() {
@@ -95,7 +162,6 @@ class Questions extends Component {
   countdown() {
     const ONE_SECOND = 1000;
     const THIRTY_SECONDS = 30000;
-
     this.interval = setInterval(() => {
       this.setState((prevState) => ({
         time: prevState.time - 1,
@@ -113,7 +179,6 @@ class Questions extends Component {
   removeFirstQuestion() {
     const { questions } = this.state;
     const meuArray = [...questions];
-
     if (meuArray.length > 1) {
       meuArray.shift();
       this.setState({
@@ -157,4 +222,23 @@ class Questions extends Component {
   }
 }
 
-export default Questions;
+const mapDispatchToProps = (dispatch) => ({
+  correctAnswer: (payload) => dispatch(actionCorrectAnswer(payload)),
+});
+
+const mapStateToProps = (state) => ({
+  score: state.gameReducer.score,
+  assertions: state.gameReducer.assertions,
+  name: state.loginReducer.name,
+  gravatarEmail: state.loginReducer.email,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Questions);
+
+Questions.propTypes = {
+  correctAnswer: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import loginReducer from './login';
 import tokenReducer from './token';
+import gameReducer from './gameReducer';
 
 const rootReducer = combineReducers({
   loginReducer,
   tokenReducer,
+  gameReducer,
 });
 
 export default rootReducer;
