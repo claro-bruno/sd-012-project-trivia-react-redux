406debb6caf818dac2e6baf69e4624c8d8fa8d27
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-08-09 22:09:41 -0300

    Descomenta codigos da page de feedbacks

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,68 +1,75 @@
 import React from 'react';
-// import Header from '../components/Header';
+import Header from '../components/Header';
 
-// const ASSERTION_AVERAGE = 3;
+const ASSERTION_AVERAGE = 3;
 
 class Feedback extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  //   this.handleUser = this.handleUser.bind(this);
-  //   this.handleRanking = this.handleRanking.bind(this);
-  // }
+    this.handleUser = this.handleUser.bind(this);
+    this.handleRanking = this.handleRanking.bind(this);
+  }
 
-  // componentDidMount() {
-  //   this.handleRanking();
-  // }
+  componentDidMount() {
+    this.handleRanking();
+  }
 
-  // handleUser() {
-  //   const userInfo = localStorage.getItem('player');
+  handleUser() {
+    const userInfo = localStorage.getItem('player');
+    const playerInfo = JSON.parse(userInfo);
 
-  //   const { name, assertions, score, gravatarEmail } = userInfo;
+    const { name, assertions, score, gravatarEmail } = playerInfo;
 
-  //   return (name, assertions, score, gravatarEmail);
-  // }
+    return (name, assertions, score, gravatarEmail);
+  }
 
-  // handleRanking() {
-  //   const storedRank = localStorage.getItem('ranking');
-  //   const ranking = JSON.parse(storedRank);
-  //   const { name, score, gravatarEmail } = this.handleUser();
-  //   const updateRank = [...ranking, {
-  //     name,
-  //     score,
-  //     picture: gravatarEmail,
-  //   }];
+  handleRanking() {
+    const storedRank = localStorage.getItem('ranking');
+    if (storedRank.length === 0) {
+      const { name, score, gravatarEmail } = this.handleUser();
+      const updateRank = [{
+        name,
+        score,
+        picture: gravatarEmail,
+      }];
+      localStorage.setItem('ranking', JSON.stringify(updateRank));
+    }
+    const ranking = JSON.parse(storedRank);
+    const { name, score, gravatarEmail } = this.handleUser();
+    const updateRank = [...ranking, {
+      name,
+      score,
+      picture: gravatarEmail,
+    }];
 
-  //   localStorage.setItem('ranking', JSON.stringify(updateRank));
-  // }
+    localStorage.setItem('ranking', JSON.stringify(updateRank));
+  }
 
   render() {
-    // const {
-    //   handleUser: {
-    //     assertions,
-    //     score,
-    //   },
-    // } = this;
+    const {
+      handleUser: {
+        assertions,
+        score,
+      },
+    } = this;
     return (
       <>
-        {/* <Header /> */}
-        {/* <h2 data-testid="feedback-text">
+        <Header />
+        <h2 data-testid="feedback-text">
           {
             (assertions < ASSERTION_AVERAGE ? 'Podia ser melhor...' : 'Mandou bem!')
           }
         </h2>
         <p data-testid="feedback-total-score">
-          `Sua pontuação final foi: $
+          Sua pontuação final foi:
           { score }
-          `
         </p>
         <p data-testid="feedback-total-question">
-          `Você acertou $
+          Você acertou
           { assertions }
           questões
-          `
-        </p> */}
-        <h1>ola</h1>
+        </p>
       </>
     );
   }
