63c86b7010dce001283f12b0783c96383ac2642a
Roberval Filho <rslfilho@gmail.com>
2021-08-10 10:41:14 -0300

    Concluindo salvando informações no Redux e LocalStorage

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/Questions.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { actionCorrectAnswer } from '../redux/actions';
 
 const MINUS_ONE = -1;
 const NUMBER_THREE = 3;
@@ -20,11 +23,29 @@ class Questions extends Component {
     this.countdown = this.countdown.bind(this);
     this.calculateScore = this.calculateScore.bind(this);
     this.getDifficultyPoints = this.getDifficultyPoints.bind(this);
+    this.setInitialLocalStorage = this.setInitialLocalStorage.bind(this);
+    this.saveScoreRedux = this.saveScoreRedux.bind(this);
+    this.saveScoreLocalStorage = this.saveScoreLocalStorage.bind(this);
   }
 
   componentDidMount() {
     this.getUnities();
     this.countdown();
+    this.setInitialLocalStorage();
+  }
+
+  setInitialLocalStorage() {
+    const { score, assertions, name, gravatarEmail } = this.props;
+    const storedInfo = {
+      player: {
+        name,
+        assertions,
+        score,
+        gravatarEmail,
+      },
+    };
+
+    localStorage.setItem('state', JSON.stringify(storedInfo));
   }
 
   async getUnities() {
@@ -55,7 +76,7 @@ class Questions extends Component {
   handleClick({ target }) {
     this.setState({ disabled: true, next: true }, () => target.classList.add('selected'));
     clearInterval(this.interval);
-    console.log(this.calculateScore(target));
+    this.saveScoreRedux(target);
   }
 
   calculateScore(target) {
@@ -69,8 +90,25 @@ class Questions extends Component {
     return 0;
   }
 
-  saveScore() {
+  saveScoreRedux(target) {
+    const { correctAnswer } = this.props;
+    const score = this.calculateScore(target);
+    correctAnswer(score);
+    this.saveScoreLocalStorage(score);
+  }
 
+  saveScoreLocalStorage(points) {
+    const { score, assertions, name, gravatarEmail } = this.props;
+    const storedInfo = {
+      player: {
+        name,
+        assertions: assertions + 1,
+        score: score + points,
+        gravatarEmail,
+      },
+    };
+
+    localStorage.setItem('state', JSON.stringify(storedInfo));
   }
 
   answersRender() {
@@ -154,4 +192,23 @@ class Questions extends Component {
   }
 }
 
-export default Questions;
+const mapDispatchToProps = (dispatch) => ({
+  correctAnswer: (payload) => dispatch(actionCorrectAnswer(payload)),
+});
+
+const mapStateToProps = (state) => ({
+  score: state.gameReducer.score,
+  assertions: state.gameReducer.assertions,
+  name: state.loginReducer.name,
+  gravatarEmail: state.loginReducer.email,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Questions);
+
+Questions.propTypes = {
+  correctAnswer: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
