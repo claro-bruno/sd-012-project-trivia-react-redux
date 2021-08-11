dee5c3a08da08300f683f27244f9fa888d2c8e30
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-08-11 11:49:05 -0300

    refatora map das alternativas

--- src/components/gameControlled/SectionQuestions.js
+++ src/components/gameControlled/SectionQuestions.js
@@ -5,6 +5,12 @@ import Button from '../Button';
 import { questionsFetchAPI } from '../../redux/actions';
 
 class SectionQuestions extends React.Component {
+  constructor() {
+    super();
+
+    this.shuffleAnswers = this.shuffleAnswers.bind(this);
+  }
+
   async componentDidMount() {
     const {
       props: { token, setQuestions },
@@ -13,47 +19,59 @@ class SectionQuestions extends React.Component {
     await setQuestions(token);
   }
 
+  shuffleAnswers(correct, incorrect, difficulty) {
+    const { props: { correctClick, wrongClick, color } } = this;
+    // referencias:
+    // https://github.com/tryber/sd-012-project-trivia-react-redux/pull/78/files
+    // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+    return [
+      {
+        answer: correct,
+        testid: 'correct-answer',
+        handleClick: () => correctClick(difficulty),
+        className: color ? 'correctColor' : null,
+      },
+      [...incorrect.map((item, index) => ({
+        answer: item,
+        testid: `wrong-answer-${index}`,
+        handleClick: wrongClick,
+        className: color ? 'wrongColor' : null,
+      }))],
+    ].sort((a, b) => a.answer.localeCompare(b.answer));
+  }
+
   render() {
     const {
-      props: {
-        questionPosition,
-        correctClick,
-        wrongClick,
-        questionsDisable,
-        color,
-        count,
-        questions,
-      },
+      props: { questionPosition, questionsDisable, count, questions },
     } = this;
 
     if (questions.length === 0) {
       return <h2>Loading...</h2>;
     }
+
+    const question = questions[questionPosition];
+
     return (
       <section>
         <h2>{ count }</h2>
-        <h2 data-testid="question-category">{ questions[questionPosition].category }</h2>
-        <h3 data-testid="question-text">{ questions[questionPosition].question }</h3>
+        <h2 data-testid="question-category">{ question.category }</h2>
+        <h3 data-testid="question-text">{ question.question }</h3>
         <section>
           {
-            questions[questionPosition].incorrect_answers.map((answers, index) => (
+            this.shuffleAnswers(
+              question.correct_answer,
+              question.incorrect_answer,
+              question.difficulty,
+            ).map(({ answer, testid, handleClick, className }) => (
               <Button
-                testId={ `wrong-answer-${index}` }
-                key={ answers }
-                name={ answers }
-                handleClick={ wrongClick }
+                testId={ testid }
+                key={ answer }
+                name={ answer }
                 disabled={ questionsDisable }
-                className={ color ? 'wrongColor' : null }
-              />
-            ))
+                handleClick={ handleClick }
+                className={ className }
+              />))
           }
-          <Button
-            testId="correct-answer"
-            name={ questions[questionPosition].correct_answer }
-            handleClick={ () => correctClick(questions[questionPosition].difficulty) }
-            disabled={ questionsDisable }
-            className={ color ? 'correctColor' : null }
-          />
         </section>
       </section>
     );
