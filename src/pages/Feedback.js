7c9d7599072821a8ff11c55d85687e7b2165dee2
Renan Ferreira <renansantiago2013@hotmail.com>
2021-08-11 15:21:04 -0300

    Ajusta o css

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -80,7 +80,7 @@ class FeedBack extends React.Component {
             Pontuação:
             <span data-testid="feedback-total-score">{score}</span>
           </p>
-          <p data-testid="feedback-text">{this.feedBackMessage()}</p>
+          <p className="feedback" data-testid="feedback-text">{this.feedBackMessage()}</p>
           <div className="buttons-container">
             <button type="button" data-testid="btn-play-again" onClick={ this.playAgain }>
               Jogar Novamente
