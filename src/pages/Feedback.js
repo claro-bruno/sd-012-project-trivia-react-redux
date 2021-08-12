6a0487c674faf63911d933151b723743a875943c
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-08-12 13:12:10 -0300

    Estilização da tela de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,6 +3,9 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import happy from '../assets/happy-brain.png';
+import sad from '../assets/sad-brain.png';
+import '../styles/feedback.css';
 
 class Feedback extends React.Component {
   componentDidMount() {
@@ -26,40 +29,59 @@ class Feedback extends React.Component {
     return 'Mandou bem!';
   }
 
+  feedBackImg() {
+    const { assertions } = this.props;
+    if (assertions >= 2) {
+      return <img className="happy" src={ happy } alt="brain" />;
+    }
+    return <img className="sad" src={ sad } alt="brain" />;
+  }
+
   render() {
     const { assertions, score } = this.props;
     return (
       <>
         <Header />
-        <p>
-          Acertos:
-          <span data-testid="feedback-total-question">
-            { assertions }
-          </span>
-        </p>
-        <p>
-          Seu score:
-          <span data-testid="feedback-total-score">
-            { score }
-          </span>
-        </p>
-        <p data-testid="feedback-text">{ this.feedbackText() }</p>
-        <Link to="/game">
-          <button
-            type="button"
-            data-testid="btn-play-again"
-          >
-            Jogar novamente
-          </button>
-        </Link>
-        <Link to="/ranking">
-          <button
-            type="button"
-            data-testid="btn-ranking"
-          >
-            Ranking
-          </button>
-        </Link>
+        {this.feedBackImg()}
+        <div className="feedback_page">
+          <div className="feedback_info">
+            <p>
+              Acertos:
+              <span data-testid="feedback-total-question">
+                {' '}
+                { assertions }
+              </span>
+            </p>
+            <p>
+              Seu score:
+              <span data-testid="feedback-total-score">
+                {' '}
+                { score }
+              </span>
+            </p>
+            <p data-testid="feedback-text">{ this.feedbackText() }</p>
+          </div>
+          <div className="feedback_btns">
+            <Link to="/game">
+              <button
+                type="button"
+                data-testid="btn-play-again"
+                className="play_again_btn_feed"
+              >
+                Jogar novamente
+              </button>
+            </Link>
+            <Link to="/ranking">
+              <button
+                type="button"
+                data-testid="btn-ranking"
+                className="rank_btn_feed"
+              >
+                Ranking
+              </button>
+            </Link>
+          </div>
+        </div>
       </>
     );
   }
