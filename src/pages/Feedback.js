5e9604c7dd3ac8314a8cf9f8aef8c0cda0e46677
vinicius-patriota <82226976+vinicius-patriota@users.noreply.github.com>
2021-08-12 15:12:39 -0300

    feedback css (#777)
    Feito o CSS do Feedback.

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import './styles/feedback.css';
 
 class Feedback extends React.Component {
   componentDidMount() {
@@ -25,28 +26,49 @@ class Feedback extends React.Component {
     const { score, assertions } = this.props;
     const minimalGuess = 3;
     return (
-      <div>
-        <Header />
-        <h1>Feedback</h1>
-        <h2
-          data-testid="feedback-text"
-        >
-          { assertions < minimalGuess ? 'Podia ser melhor...' : 'Mandou bem!' }
-        </h2>
-        <div>
-          <h3>Pontuação</h3>
-          <p data-testid="feedback-total-score">{ score }</p>
+      <div className="feedback-content">
+        <div className="feedback-container">
+          <Header />
+          <h1 className="feedback-title">Feedback</h1>
+          <h2
+            data-testid="feedback-text"
+            className="feedback-text"
+          >
+            { assertions < minimalGuess ? 'Podia ser melhor...' : 'Mandou bem!' }
+          </h2>
+          <div className="feedback-results">
+            <h3>Pontuação</h3>
+            <p
+              data-testid="feedback-total-score"
+            >
+              { score }
+            </p>
+          </div>
+          <div className="feedback-results">
+            <h3>Acertos</h3>
+            <p
+              data-testid="feedback-total-question"
+            >
+              { assertions }
+            </p>
+          </div>
+          <Link className="btn" to="/">
+            <button
+              type="button"
+              data-testid="btn-play-again"
+            >
+              Jogar novamente
+            </button>
+          </Link>
+          <Link className="btn" to="/ranking">
+            <button
+              type="button"
+              data-testid="btn-ranking"
+            >
+              Ver Ranking
+            </button>
+          </Link>
         </div>
-        <div>
-          <h3>Acertos</h3>
-          <p data-testid="feedback-total-question">{ assertions }</p>
-        </div>
-        <Link to="/">
-          <button type="button" data-testid="btn-play-again">Jogar novamente</button>
-        </Link>
-        <Link to="/ranking">
-          <button type="button" data-testid="btn-ranking">Ver Ranking</button>
-        </Link>
       </div>
     );
   }
