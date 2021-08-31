fe683251016c9d30df76d1b32d0c271dbf9b17d9
johnpligeiro <johnpligeiro@gmail.com>
2021-08-31 10:13:15 -0300

    feat: req 2

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-
+// teste não local não passa a store / req1
 const store = createStore(
   rootReducer,
   composeWithDevTools(
