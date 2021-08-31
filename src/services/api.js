5eb2d11314a08fef0b967b80717afdec4cb90fc1
johnpligeiro <johnpligeiro@gmail.com>
2021-08-31 10:47:46 -0300

    feat: req 5 + 6

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
-const fetchToken = async () => {
-  const response = await fetch('https://opentdb.com/api_token.php?command=request');
+export const fetchToken = async () => {
+  const endpoint = 'https://opentdb.com/api_token.php?command=request';
+  const response = await fetch(endpoint);
   const data = await response.json();
   return data.token;
 };
 
-export default fetchToken;
+export const fetchQuestions = async (token) => {
+  const endpoint = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data.results;
+};
