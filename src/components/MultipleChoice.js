4697113525055ff2216d6caae96bfb6808839a80
johnpligeiro <johnpligeiro@gmail.com>
2021-08-31 11:15:35 -0300

    feat: req 7

--- src/components/MultipleChoice.js
+++ src/components/MultipleChoice.js
@@ -6,11 +6,22 @@ const caseTrue = 1;
 const caseFalse = -1;
 
 class MultipleChoice extends React.Component {
+  changeColor({ target }) {
+    const getButtons = target.parentElement.children;
+
+    for (let index = 0; index < getButtons.length; index += 1) {
+      if (getButtons[index].dataset.testid === 'correct-answer') {
+        getButtons[index].classList.add('correct');
+      } else {
+        getButtons[index].classList.add('wrong');
+      }
+    }
+  }
+
   render() {
     const { question } = this.props;
     const answers = [question.correct_answer, ...question.incorrect_answers];
 
-    // stackoverflow
     answers.sort(() => (Math.random() > fiftyPercent ? caseTrue : caseFalse));
 
     return (
@@ -20,6 +31,7 @@ class MultipleChoice extends React.Component {
             <button
               key={ index }
               type="button"
+              onClick={ this.changeColor }
               data-testid={
                 question.incorrect_answers.includes(answer)
                   ? `wrong-answer-${question.incorrect_answers.indexOf(answer)}`
