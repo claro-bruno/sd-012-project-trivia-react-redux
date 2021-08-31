532f230b0276a34131cb1cf0140fc38c245a8d1f
johnpligeiro <johnpligeiro@gmail.com>
2021-08-31 12:38:48 -0300

    feat: req 13 - feedback mesage

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+
+const scoreMedia = 3;
 
 class Feedback extends React.Component {
   render() {
+    const { assertions } = this.props;
+
     return (
       <div>
         <Header />
         <h1 data-testid="feedback-text"> Página de Feedback </h1>
+        <p data-testid="feedback-text">
+          {
+            assertions >= scoreMedia
+              ? 'Mandou bem!'
+              : 'Podia ser melhor...'
+          }
+        </p>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = ({ gameReducer: { assertions } }) => ({
+  assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
+
+Feedback.propTypes = {
+  assertions: PropTypes.number,
+}.isRequired;
