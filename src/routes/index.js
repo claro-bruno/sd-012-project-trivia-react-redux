0b174d4f93b3596fa719ce55e3cc84b79249edf0
johnpligeiro <johnpligeiro@gmail.com>
2021-08-31 12:23:26 -0300

    feat: feedback page and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Game from '../pages/Game';
 import Settings from '../pages/Settings';
+import Feedback from '../pages/Feedback';
 
 class Routes extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Routes extends React.Component {
         <Route exact path="/" component={ Home } />
         <Route path="/game" component={ Game } />
         <Route path="/settings" component={ Settings } />
+        <Route path="/feedback" component={ Feedback } />
       </Switch>
     );
   }
